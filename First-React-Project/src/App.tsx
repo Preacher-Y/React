9c893cd7fbfd8157ac1f187f6528c5d946456304
Preacher-Y <ysheja@gmail.com>
2025-07-03 15:13:07 +0200

    few adjustments and adding footer

--- First-React-Project/src/App.tsx
+++ First-React-Project/src/App.tsx
@@ -31,4 +31,22 @@ export function Body() {
   )
 }
 
+export function Footer() {
+  return (
+    <>
+      <footer className='flex absolute w-full bottom-0 justify-center items-center bg-[#20222B] h-16'>
+        <small className='text-[#01D8FE] font-bold text-lg'>© 2023 ReactFacts. All rights reserved.</small>
+      </footer>
+    </>
+  )
+}
 
+export default function  App() {
+  return (
+    <>
+      <Nav />
+      <Body />
+      <Footer />
+    </>
+  )
+}
--- First-React-Project/src/main.tsx
+++ First-React-Project/src/main.tsx
@@ -2,12 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import reactIcon from "./assets/reactIcon.png"
 import './index.css'
-import {Body, Nav} from './App.tsx'
+import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Nav />
-    <img src={reactIcon} className="absolute top-24 right-0 -z-20" alt="React logo" />
-    <Body />
+    <img src={reactIcon} className="absolute top-24 right-0 -z-10" alt="React logo" />
+    <App />
   </StrictMode>,
 )
