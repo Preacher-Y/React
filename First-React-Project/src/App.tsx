3452dfead42a2a2b8269199219f9dfe174615f50
Preacher-Y <ysheja@gmail.com>
2025-07-03 11:56:53 +0200

    chore: add package.json with Tailwind CSS and Vite dependencies

---
+++ First-React-Project/src/App.tsx
@@ -0,0 +1,24 @@
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+export function Nav() { 
+  return (
+    <>
+      <div>
+        <div className='flex items-center shadow-md bg-[#20222B] gap-2 p-5'>
+          <img src={reactLogo} alt="" className='h-12' />
+          <h1 className='text-[#01D8FE] font-bold text-2xl'>ReactFacts</h1>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export function Body() {
+  return (
+    <>
+    
+    </>
+  )
+}
+
