8bf0193e309a7236a91e89068b1c07f28e105f1c
Preacher-Y <ysheja@gmail.com>
2025-07-06 15:27:14 +0200

    fix: update list style in Body component for improved visual consistency

--- First-React-Project/src/components/body.tsx
+++ First-React-Project/src/components/body.tsx
@@ -5,7 +5,7 @@ export default function Body() {
     <div>
       <div className='text-xl md:text-2xl z-1 text-white mx-7 my-2 md:mx-10 md:my-4'>
         <h1 className='font-bold text-3xl md:text-5xl'>Fun facts about React</h1>
-        <ul className='list-disc mt-4 ml-10'>
+        <ul className='list-disc marker:text-[#01D8FE] mt-4 ml-10'>
           <li>Was first released in 2013</li>
           <li>Was originally created by Jordan Walke</li>
           <li>Has well over 200K stars on Github</li>
