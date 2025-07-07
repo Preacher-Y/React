3d141ba57e4fc3d040a470c8b6cd20bd0579ee95
Preacher-Y <ysheja@gmail.com>
2025-07-07 10:19:51 +0200

    refactor: replace Lucide icons with React Icons in Header and Body components; update package.json and package-lock.json

--- Second-React-Project/src/components/body.tsx
+++ Second-React-Project/src/components/body.tsx
@@ -1,4 +1,4 @@
-import { LucideLocationEdit } from "lucide-react";
+import { IoLocationSharp } from "react-icons/io5";
 import Data from "../data";
 
 export default function Body() {
@@ -8,9 +8,9 @@ export default function Body() {
                 <div className="bg-white flex flex-col md:flex-row md:space-x-5 shadow-md rounded-lg p-4 mb-4 space-y-4 md:space-y-0">
                     <img src={el.img.src} alt={el.img.alt} className="w-full md:w-42 h-60 md:h-auto object-cover rounded-lg"/>
                     <div className="flex-1 flex flex-col justify-center mb-2">
-                        <div className="flex flex-col md:flex-row md:items-end md:space-x-5 space-y-2 md:space-y-0">
-                            <div className="flex items-center space-x-2">
-                                <LucideLocationEdit className="h-5 text-[#F55A5A]" />
+                        <div className="flex flex-col md:flex-row md:items-center md:space-x-5 space-y-2 md:space-y-0">
+                            <div className="flex items-center space-x-1">
+                                <IoLocationSharp className="h-14 text-[#F55A5A]" />
                                 <h2 className="text-sm font-medium text-gray-700 uppercase tracking-[4px]">{el.country}</h2>
                             </div>
                             <a href={el.googleMapsLink} target="_blank" rel="noopener noreferrer" className="underline text-gray-500 hover:text-gray-700">
--- Second-React-Project/src/components/header.tsx
+++ Second-React-Project/src/components/header.tsx
@@ -1,8 +1,8 @@
-import { Globe2 } from 'lucide-react'
+import { LiaGlobeSolid } from "react-icons/lia";
 export default function Header() {
   return (
     <header className="bg-[#F55A5A] w-full flex justify-center text-center text-white p-4">
-        <Globe2 className="inline-block mr-2" size={24} />
+        <LiaGlobeSolid className="inline-block mr-2" size={24} />
         <h1 className="font-bold">My Travel Journal</h1>
     </header>
   );
