b4f6cf867b646f8133322225ec8887db65b1a834
Preacher-Y <ysheja@gmail.com>
2025-07-10 16:10:23 +0200

    Third Project: - Done With the UI

--- Third-React-Project(ChefClaude)/src/components/body.tsx
+++ Third-React-Project(ChefClaude)/src/components/body.tsx
@@ -6,14 +6,14 @@ import Generate from './generate';
 export default function Body() {
     return (
         <div className="max-w-2xl mx-auto">
-            <section id ='hero' className="p-4 mt-18 md:mt-32 relative">
+            <section id ='hero' className="p-4 mt-32 mb-16 relative">
                 <h2 className="text-3xl sm:text-4xl md:text-5xl text-center font-bold mb-4">
                     Welcome to Chef Claude
                 </h2>
                 <p className="text-gray-700 mb-6 text-center mx-auto w-full sm:w-3/4">
                     Chef Claude is your personal AI chef, ready to assist you with all your culinary needs. Whether you're looking for recipes, cooking tips, or meal planning ideas, Chef Claude has got you covered.
                 </p>
-                <div className='flex flex-col sm:flex-row justify-center items-center mt-14 gap-4'>
+                <div className='flex flex-col sm:flex-row justify-center items-center mt-20 mb-1 gap-4'>
                     <input
                         type="text"
                         placeholder='e.g oregano'
@@ -24,6 +24,7 @@ export default function Body() {
                         color="bg-black/90"
                         size="py-2 flex items-center w-full sm:w-50 justify-center hover:-translate-y-1 hover:shadow-[0px_0px_8px_black] max-sm:mt-3 transition-all duration-500 px-4 sm:ml-4"
                         onClick={handleAddIngredient}
+
                     />
                 </div>
                 <p className='px-24 md:px-30 text-gray-500 text-xs '>at least 3 ingredients are required</p>
--- Third-React-Project(ChefClaude)/src/components/generate.tsx
+++ Third-React-Project(ChefClaude)/src/components/generate.tsx
@@ -1,17 +1,17 @@
 import Button from "./button";
 export default function Generate() {
     return (
-        <div className="bg-gray-200 rounded-2xl p-5 flex ">
-            <div className="px-4">
+        <div className="bg-gray-200 rounded-2xl p-6 flex items-end gap-2 ">
+            <div className="px-3">
                 <h1 className="font-bold text-base mb-1">Ready for a recipe?</h1>
-                <p className="text-gray-500 text-[12px]">
+                <p className="text-gray-500 text-xs">
                     Generate a delicious recipe based on the ingredients you have on hand.
                 </p>
             </div>
             <Button
                 text="Get a recipe"
-                color="bg-orange-400"
-                size="py-2 px-4 scale-70 flex items-center content-end hover:scale-80 hover:shadow-[0px_0px_8px_darkOrange] transition-all duration-500"
+                color="bg-red-400"
+                size="py-2 px-5 items-center text-sm hover:bg-red-400/80 transition-all duration-500"
                 onClick={() => alert("Recipe generated!")}
             />
         </div>
