f28afa09f47050a82342792e786d04c5ac4dc1ae
Preacher-Y <ysheja@gmail.com>
2025-07-12 20:39:49 +0200

    Small Ajustment

--- Third-React-Project(ChefClaude)/src/components/results.tsx
+++ Third-React-Project(ChefClaude)/src/components/results.tsx
@@ -1,5 +1,4 @@
 import { useState } from "react"
-
 import Loading from "./loading";
 export default function Results(prop:any): React.JSX.Element {
     const [loading, setLoading] = useState(true);
