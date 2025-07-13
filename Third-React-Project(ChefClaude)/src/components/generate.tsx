e7e7523478bf723a389026eef16a119a856b3ce2
Preacher-Y <ysheja@gmail.com>
2025-07-13 19:31:16 +0200

    Third Project: - Small Adjustment

--- Third-React-Project(ChefClaude)/src/components/generate.tsx
+++ Third-React-Project(ChefClaude)/src/components/generate.tsx
@@ -4,7 +4,7 @@ export default function Generate() {
     const { ingredients } = useIngredients();
     return (
         <>
-        {ingredients.length >= 3 &&
+        {ingredients.length>= 3 &&
             (<div className="bg-gray-200 dark:bg-gray-800 rounded-2xl p-6 flex transition-all duration-500 items-end gap-2 ">
                 <div className="px-3">
                     <h1 className="font-bold dark:text-gray-300 text-base mb-1">Ready for a recipe?</h1>
