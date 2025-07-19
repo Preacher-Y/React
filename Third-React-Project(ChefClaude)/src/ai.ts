5395780cdb2e02ee9654f7654c1902afa1fb7cd1
Preacher-Y <ysheja@gmail.com>
2025-07-19 16:02:58 +0200

    Third Project: - Modifying the Ai propmpt

--- Third-React-Project(ChefClaude)/src/ai.ts
+++ Third-React-Project(ChefClaude)/src/ai.ts
@@ -13,9 +13,9 @@ they mention in your recipe. The recipe can include additional ingredients they 
 but try not to include too many extra ingredients. give it me in an object format with the following keys:
 - name: the name of the recipe
 - description: a short description of the recipe
-- ingredients: a list of ingredients with measurements(an array)
+- ingredients: a list of ingredients with measurements(an array) incluing the one that didn't add in
 - instructions: a list of instructions to make the recipe(an array)
-- image: a URL to an image of the recipe
+- image: a URL to an image of the recipe and make sure the image url works
 Make sure to include the measurements for each ingredient, and provide clear and concise instructions.
 If you don't have enough information to suggest a recipe, respond with "I don't know".
 `;
