70ff037e67b313db1b030e40ba6d2331c54b5663
Preacher-Y <ysheja@gmail.com>
2025-07-20 12:38:59 +0200

    Fourth Project: - Added State  in the App.tsx

--- Fourth-React-Project(meme)/src/App.tsx
+++ Fourth-React-Project(meme)/src/App.tsx
@@ -1,11 +1,23 @@
 import Header from "./components/header"
 import Body from "./components/body"
-export default function App() {
+import Results from "./components/results"
+import { useState } from "react"
 
+export type DataType = {
+  topText: string;
+  bottomText: string;
+};
+
+export default function App() {
+  const [data,setData] = useState<DataType>({
+    topText:'',
+    bottomText:''
+  })
   return (
     <>
       <Header/>
-      <Body/>
+      <Body setData={setData}/>
+      <Results data={data}/>
     </>
   )
 }
