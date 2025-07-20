f7c8496b853e262cea2e87c5d68f958aeb23254e
Preacher-Y <ysheja@gmail.com>
2025-07-20 10:22:04 +0200

    Third Project: - Small Adjustment on the header component

--- Third-React-Project(ChefClaude)/src/components/header.tsx
+++ Third-React-Project(ChefClaude)/src/components/header.tsx
@@ -5,8 +5,10 @@ export default function Header() {
     return (
         <header className="grid justify-center dark:bg-gray-900 transition-all duration-500 bg-white fixed z-10 w-full top-0 rounded-b-2xl items-center p-4">
             <div className="flex items-center">
-                <img src={Logo} alt="Chef Claude Logo" className="h-10 w-9 mr-2" />
-                <h1 className="text-2xl dark:text-white font-light">Chef Claude</h1>
+                <a href="/" className='flex items-center'>
+                    <img src={Logo} alt="Chef Claude Logo" className="h-10 w-9 mr-2" />
+                    <h1 className="text-2xl dark:text-white font-light">Chef Claude</h1>
+                </a>
                 <Theme />
             </div>
         </header>
