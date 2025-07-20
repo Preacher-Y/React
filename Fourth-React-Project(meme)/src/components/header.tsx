23f0a047c74e08c2363f85b8efff89a165beb1ad
Preacher-Y <ysheja@gmail.com>
2025-07-20 10:19:04 +0200

    Fourth Project: - Small Adjustment on the Header component

--- Fourth-React-Project(meme)/src/components/header.tsx
+++ Fourth-React-Project(meme)/src/components/header.tsx
@@ -1,9 +1,11 @@
 import Logo from '../assets/Troll Face.svg'
 export default function (){
     return (
-        <header className='flex items-center px-6 py-3 bg-gradient-to-r from-[#672280] to-[#A626D3] gap-1'>
-            <img src={Logo} alt="meme Logo" />
-            <h1 className='text-white font-semibold'>Meme Generator</h1>
+        <header className='px-6 py-3 bg-gradient-to-r from-[#672280] to-[#A626D3]'>
+            <a href="/" className='flex items-center gap-1'>
+                <img src={Logo} alt="meme Logo" />
+                <h1 className='text-white font-semibold'>Meme Generator</h1>
+            </a>
         </header>
     )
 }
