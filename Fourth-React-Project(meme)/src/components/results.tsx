28f2903ef06f5c397cbeb326e3a2fcd516f10caa
Preacher-Y <ysheja@gmail.com>
2025-07-22 16:00:53 +0200

    Fourth Project: - Adding Good Styling

--- Fourth-React-Project(meme)/src/components/results.tsx
+++ Fourth-React-Project(meme)/src/components/results.tsx
@@ -1,9 +1,39 @@
 import type { DataType } from '../App'
 export default function Results({data}:{data:DataType}){
     return (
-        <section className='text-center'>
-            <h1>{data.topText}</h1>
-            <h2>{data.bottomText}</h2>
+        <section className=''>
+            <h1 className=' text-center my-[15px] px-[5px] font-[impact,sans-serif] text-[2rem] uppercase text-white tracking-[1px]' 
+            style={
+                {
+                    textShadow: `
+                    2px 2px 0 #000,
+                    -2px -2px 0 #000,
+                    2px -2px 0 #000,
+                    -2px 2px 0 #000,
+                    0 2px 0 #000,
+                    2px 0 0 #000,
+                    0 -2px 0 #000,
+                    -2px 0 0 #000,
+                    2px 2px 5px #000
+                    `
+                }
+            }>{data.topText}</h1>
+            <h2 className=' text-center my-[15px] px-[5px] font-[impact,sans-serif] text-[2rem] uppercase text-white tracking-[1px]'
+            style={
+                {
+                    textShadow: `
+                    2px 2px 0 #000,
+                    -2px -2px 0 #000,
+                    2px -2px 0 #000,
+                    -2px 2px 0 #000,
+                    0 2px 0 #000,
+                    2px 0 0 #000,
+                    0 -2px 0 #000,
+                    -2px 0 0 #000,
+                    2px 2px 5px #000
+                    `
+                }
+            }>{data.bottomText}</h2>
         </section>
     )
 }
