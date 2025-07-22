372f2e1e6dce1de490f48a613ee8f34936da72a2
Preacher-Y <ysheja@gmail.com>
2025-07-22 16:30:06 +0200

    Fouth Project: - Getting somewhere

--- Fourth-React-Project(meme)/src/components/results.tsx
+++ Fourth-React-Project(meme)/src/components/results.tsx
@@ -1,10 +1,17 @@
 import type { DataType } from '../App'
 export default function Results({data}:{data:DataType}){
     return (
-        <section className={`my-10 bg-[url('http://i.imgflip.com/1bij.jpg')] bg-contain bg-no-repeat bg-center`}>
-            <h1 className=' text-center my-[15px] px-[5px] font-[impact,sans-serif] text-3xl uppercase text-white tracking-[1px]' 
-            style={
-                {
+        <section className="my-10 flex justify-center">
+            <div className="relative w-[512px] h-[342px]">
+                <img
+                src="http://i.imgflip.com/1bij.jpg"
+                alt="Meme"
+                className="w-full h-full object-cover"
+                />
+   
+                <h1
+                className="absolute top-8 left-1/2 -translate-x-1/2 font-[impact,sans-serif] text-2xl uppercase text-white tracking-[1px]"
+                style={{
                     textShadow: `
                     2px 2px 0 #000,
                     -2px -2px 0 #000,
@@ -15,12 +22,12 @@ export default function Results({data}:{data:DataType}){
                     0 -2px 0 #000,
                     -2px 0 0 #000,
                     2px 2px 5px #000
-                    `
-                }
-            }>{data.topText}</h1>
-            <h2 className=' text-center my-22 px-[5px] font-[impact,sans-serif] text-3xl uppercase text-white tracking-[1px]'
-            style={
-                {
+                    `,
+                }}>{data.topText}</h1>
+
+                <h2
+                className="absolute bottom-8 left-1/2 -translate-x-1/2 px-2 font-[impact,sans-serif] text-2xl uppercase text-white tracking-[1px]"
+                style={{
                     textShadow: `
                     2px 2px 0 #000,
                     -2px -2px 0 #000,
@@ -31,9 +38,10 @@ export default function Results({data}:{data:DataType}){
                     0 -2px 0 #000,
                     -2px 0 0 #000,
                     2px 2px 5px #000
-                    `
-                }
-            }>{data.bottomText}</h2>
+                    `,
+                }}>{data.bottomText}</h2>
+            </div>
         </section>
+
     )
 }
