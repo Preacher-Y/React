700e03f2a68f20eaf5cb68da0ddce8155281bccf
Preacher-Y <ysheja@gmail.com>
2025-07-22 16:10:36 +0200

    Fourth Project: - Centering the Picture - and resizing the words

--- Fourth-React-Project(meme)/src/components/results.tsx
+++ Fourth-React-Project(meme)/src/components/results.tsx
@@ -1,8 +1,8 @@
 import type { DataType } from '../App'
 export default function Results({data}:{data:DataType}){
     return (
-        <section className=''>
-            <h1 className=' text-center my-[15px] px-[5px] font-[impact,sans-serif] text-[2rem] uppercase text-white tracking-[1px]' 
+        <section className={`my-10 bg-[url('http://i.imgflip.com/1bij.jpg')] bg-contain bg-no-repeat bg-center`}>
+            <h1 className=' text-center my-[15px] px-[5px] font-[impact,sans-serif] text-3xl uppercase text-white tracking-[1px]' 
             style={
                 {
                     textShadow: `
@@ -18,7 +18,7 @@ export default function Results({data}:{data:DataType}){
                     `
                 }
             }>{data.topText}</h1>
-            <h2 className=' text-center my-[15px] px-[5px] font-[impact,sans-serif] text-[2rem] uppercase text-white tracking-[1px]'
+            <h2 className=' text-center my-22 px-[5px] font-[impact,sans-serif] text-3xl uppercase text-white tracking-[1px]'
             style={
                 {
                     textShadow: `
