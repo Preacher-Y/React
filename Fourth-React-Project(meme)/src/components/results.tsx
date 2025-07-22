8850da3b3c8d9bfd7caadd541a2e0aaf8d4267df
Preacher-Y <ysheja@gmail.com>
2025-07-22 16:38:04 +0200

    Fourth Project - Done with the Styling

--- Fourth-React-Project(meme)/src/components/results.tsx
+++ Fourth-React-Project(meme)/src/components/results.tsx
@@ -1,16 +1,16 @@
 import type { DataType } from '../App'
 export default function Results({data}:{data:DataType}){
     return (
-        <section className="my-10 flex justify-center">
-            <div className="relative w-[512px] h-[342px]">
+       <section className="my-10 flex justify-center">
+            <div className="relative w-full max-w-[512px] max-sm:mx-7 aspect-[3/2]">
                 <img
                 src="http://i.imgflip.com/1bij.jpg"
                 alt="Meme"
                 className="w-full h-full object-cover"
                 />
-   
+                
                 <h1
-                className="absolute top-8 left-1/2 -translate-x-1/2 font-[impact,sans-serif] text-2xl uppercase text-white tracking-[1px]"
+                className="absolute top-4 left-1/2 -translate-x-1/2 px-1 font-[impact,sans-serif] text-nowrap text-xl sm:text-2xl md:text-3xl lg:text-4xl uppercase text-white tracking-[1px]"
                 style={{
                     textShadow: `
                     2px 2px 0 #000,
@@ -26,7 +26,7 @@ export default function Results({data}:{data:DataType}){
                 }}>{data.topText}</h1>
 
                 <h2
-                className="absolute bottom-8 left-1/2 -translate-x-1/2 px-2 font-[impact,sans-serif] text-2xl uppercase text-white tracking-[1px]"
+                className="absolute bottom-4 left-1/2 -translate-x-1/2 px-1 font-[impact,sans-serif] text-nowrap text-xl sm:text-2xl md:text-3xl lg:text-4xl uppercase text-white tracking-[1px]"
                 style={{
                     textShadow: `
                     2px 2px 0 #000,
@@ -42,6 +42,7 @@ export default function Results({data}:{data:DataType}){
                 }}>{data.bottomText}</h2>
             </div>
         </section>
+
 
     )
 }
