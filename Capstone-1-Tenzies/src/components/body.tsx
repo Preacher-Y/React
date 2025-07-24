e3a5d359adb94216664581deabb904cc0be513e0
Preacher-Y <ysheja@gmail.com>
2025-07-24 16:30:39 +0200

    Capstone Project-1: - Adding the All Dice component to the body - modifying the header component

---
+++ Capstone-1-Tenzies/src/components/body.tsx
@@ -0,0 +1,12 @@
+import AllDices from "./allDices"
+
+export default function Body(){
+    return(
+        <>
+            <h1 className="text-center">
+                Roll dice 
+            </h1>
+            <AllDices/>
+        </>
+    )
+}
--- Capstone-1-Tenzies/src/components/header.tsx
+++ Capstone-1-Tenzies/src/components/header.tsx
@@ -3,7 +3,7 @@ import { memo, type JSX } from "react"
 function Header():JSX.Element{
     return(
         <>
-            <header className="bg-white text-center pt-9">
+            <header className="text-center pt-9">
                 <h1 className="text-4xl">Tenzies</h1>
             </header>
         </>
