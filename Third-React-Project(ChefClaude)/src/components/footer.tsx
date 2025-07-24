87488fa88e7994b6aa3e71e0dd8c5284f39b8e2a
Preacher-Y <ysheja@gmail.com>
2025-07-24 17:10:56 +0200

    Third Project: - Small changes on the footer

--- Third-React-Project(ChefClaude)/src/components/footer.tsx
+++ Third-React-Project(ChefClaude)/src/components/footer.tsx
@@ -1,7 +1,7 @@
 
 export default function Footer(){
     return (
-        <footer className="grid justify-center md:fixed lg:relative dark:bg-gray-900 transition-all duration-500 bg-white z-10 w-full bottom-0 rounded-t-2xl items-center p-4">
+        <footer className="grid justify-center md:fixed dark:bg-gray-900 transition-all duration-500 bg-white z-10 w-full bottom-0 rounded-t-2xl items-center p-4">
             <div className="flex items-center">
                 <p className="text-xs dark:text-white font-medium ">© 2023 Chef Claude 
                     <span className="hover:text-blue-900 dark:hover:text-blue-300 hover:underline underline-offset-3 ml-2 ">
