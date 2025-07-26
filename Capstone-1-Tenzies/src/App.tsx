e12060537feb657f62b3791b7512d9d331311f96
Preacher-Y <ysheja@gmail.com>
2025-07-26 16:46:43 +0200

    Capstone Projec-1: - Adding so that the hold shall remain after rolling the dice

--- Capstone-1-Tenzies/src/App.tsx
+++ Capstone-1-Tenzies/src/App.tsx
@@ -27,12 +27,16 @@ export default function App() {
         ():Die =>({value: Math.ceil(Math.random() * 6),isHeld:false,id:nanoid()})
     )
   }
+
+
   return (
     <div className="border-40 w-full h-[100dvh] ">
       <Header/>
       <section className="max-w-lg mx-auto h-96 flex flex-col gap-3 justify-center">
         <Body dice={dice} toggleHold={toggleHold}/>
-        <button onClick={()=>setDice(generateAllNewDice)} className="bg-blue-600 text-white w-46 rounded-2xl text-xl self-center mt-10 py-2 px-4">Roll the Dice</button>
+        <button 
+        onClick={()=>setDice(prev=>prev.map(el=>el.isHeld?el:{...el,value:Math.ceil(Math.random() * 6)}))} 
+        className="bg-blue-600 text-white w-46 rounded-2xl text-xl self-center mt-10 py-2 px-4">Roll the Dice</button>
       </section>
     </div>
   )
