cce10702aebd52d1bf357fcf93f297684c76fb73
Preacher-Y <ysheja@gmail.com>
2025-07-26 16:15:46 +0200

    Capstone Project-1: - Small tweak

--- Capstone-1-Tenzies/src/components/dices.tsx
+++ Capstone-1-Tenzies/src/components/dices.tsx
@@ -2,7 +2,7 @@
 export default function Dices({value,isHeld,id,toggleHold}:{value:number,isHeld:boolean,id:string,toggleHold:(id:string)=>void}){
     return(
         <>
-            <button className={`${isHeld?'bg-green-400':'bg-gray-100'} w-12 h-10 hover:bg-green-400 text-center rounded drop-shadow-[0px_0px_2px_gray] text-xl `} onClick={()=>{toggleHold(id)}}>{value}</button>
+            <button className={`${isHeld?'bg-green-400 hover:bg-green-800':'bg-gray-100 hover:bg-green-500'} w-12 h-10 text-center rounded drop-shadow-[0px_0px_2px_gray] text-xl `} onClick={()=>{toggleHold(id)}}>{value}</button>
         </>
     )
 }
