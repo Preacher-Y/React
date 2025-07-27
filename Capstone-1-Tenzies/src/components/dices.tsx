1a2e578584eb8dc55fbae1cc8c154e7ca635db8f
Preacher-Y <ysheja@gmail.com>
2025-07-27 14:15:40 +0200

    Capstone Project-1: - Added cursor-not allowed to the button when the game is won

--- Capstone-1-Tenzies/src/components/dices.tsx
+++ Capstone-1-Tenzies/src/components/dices.tsx
@@ -2,7 +2,7 @@
 export default function Dices({value,isHeld,id,toggleHold,isDisabled}:{value:number,isHeld:boolean,id:string,toggleHold:(id:string)=>void,isDisabled:boolean}){
     return(
         <>
-            <button disabled={isDisabled} className={`${isHeld?'bg-green-400 hover:bg-green-800':'bg-gray-100 hover:bg-green-500'} w-12 h-10 text-center rounded drop-shadow-[0px_0px_2px_gray] text-xl `} onClick={()=>{toggleHold(id)}}>{value}</button>
+            <button disabled={isDisabled} className={`${isHeld?'bg-green-400 hover:bg-green-800':'bg-gray-100 hover:bg-green-500'} ${isDisabled?'cursor-not-allowed':''} w-12 h-10 text-center rounded drop-shadow-[0px_0px_2px_gray] text-xl `} onClick={()=>{toggleHold(id)}}>{value}</button>
         </>
     )
 }
