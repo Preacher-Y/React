643384bfa60d3a831cae768b4048ab6cdd10d736
Preacher-Y <ysheja@gmail.com>
2025-07-28 23:01:42 +0200

    Capstone Project-2: - Using clsx to change the css cause it return only strings - add clsx on the keyboard generator/ components so that it will be check upon re-rendering

--- Capstone-2-EndGame/src/components/keyboard.tsx
+++ Capstone-2-EndGame/src/components/keyboard.tsx
@@ -1,23 +1,39 @@
 import { useLetterContext } from "../hooks/letterContext"
+import { useWordContext } from "../hooks/wordContext"
+import clsx from "clsx"
 
 export default function Keyboard(){
 
     const {letter,setLetter} = useLetterContext()
+    const {word} = useWordContext()
 
-    function addGuessedLetter(aletter:string):void{
-        setLetter(prev=>prev.includes(aletter)?prev:[...prev,aletter])
+    function addGuessedLetter(letter:string):void{
+        setLetter(prev=>prev.includes(letter)?prev:[...prev,letter])
     }
 
     const keys = 'qwertyuiopasdfghjklzxcvbnm'.toUpperCase()
     return (
         <section className="flex justify-center flex-wrap gap-2 mx-10 my-6">
-            {keys.split('').map(el=>
-                    <button 
+            {keys.split('').map(el=>{
+                
+                return(
+                    <button
                         key={el}
-                        onClick={()=>addGuessedLetter(el)} 
-                        className="w-14 h-14 rounded-md font-bold text-2xl place-self-center border border-spacing-10 border-white bg-yellow-500 text-[#333233] content-center">
+                        onClick={() => addGuessedLetter(el)}
+                        disabled={letter.includes(el)}
+                        className={clsx(
+                            "w-14 h-14 rounded-md font-bold text-2xl place-self-center border border-white content-center",
+                            {
+                            "bg-green-600 text-white": letter.includes(el) && word.toUpperCase().includes(el),
+                            "bg-red-600 text-white": letter.includes(el) && !word.toUpperCase().includes(el),
+                            "bg-yellow-500 text-[#333233]": !letter.includes(el),
+                            }
+                        )}
+                        >
                         {el}
-                    </button>)}
+                    </button>
+                )
+            })}
         </section>
     )
 }
