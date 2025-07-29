dc9abe3fb22a7cdc7aa504699ef1bd80fab29cf7
Preacher-Y <ysheja@gmail.com>
2025-07-29 10:13:01 +0200

    Capstone Project-2: - Adding the Logic that the word letter will always appear when the guessed letter is right

--- Capstone-2-EndGame/src/components/word.tsx
+++ Capstone-2-EndGame/src/components/word.tsx
@@ -1,12 +1,14 @@
+import { useLetterContext } from "../hooks/letterContext"
 import { useWordContext } from "../hooks/wordContext"
 
 export default function Word(){
 
     const {word} = useWordContext()
+    const {letter} = useLetterContext()
 
     return (
         <section className="flex justify-center gap-2 my-10">
-            {word.toUpperCase().split('').map(el=><span key={el} className="border-b-2 border-b-gray-500 content-center w-12 h-12 text-2xl bg-[#333233] font-bold">{el}</span>)}
+            {word.toUpperCase().split('').map(el=><span key={el} className="border-b-2 border-b-gray-500 content-center w-12 h-12 text-2xl bg-[#333233] font-bold">{letter.includes(el)?el:''}</span>)}
         </section>
     )
 }
