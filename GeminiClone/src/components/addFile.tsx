d092496c00177af1da537dbbb8694f67d0eaeca0
Preacher-Y <ysheja@gmail.com>
2025-08-03 18:13:29 +0200

    GeminiClone: - Add fille Small Tweak

--- GeminiClone/src/components/addFile.tsx
+++ GeminiClone/src/components/addFile.tsx
@@ -1,28 +1,40 @@
-import React from 'react';
+import React, { useRef } from 'react';
 
 export default function AddFile(){
     const [isclicked, setIsClicked] = React.useState(false);
+    const addFile = useRef<HTMLDivElement>(null)
 
     const handleClick = () => {
         setIsClicked(!isclicked);
     }
+
+    React.useEffect(() => {
+        const handleOutsideClick = (e: MouseEvent) => {
+            if (addFile.current && !addFile.current.contains(e.target as Node)) {
+                setIsClicked(false);
+            }
+        };
+        document.addEventListener('mousedown', handleOutsideClick);
+        return () => document.removeEventListener('mousedown', handleOutsideClick);
+    }, []);
+    
     return (
-        <>
-            <button className="hover:bg-gray-500/20 cursor-pointer flex items-center gap-1 rounded-full px-2 py-2">
-                <span className="icon-[line-md--plus] cursor-pointer text-xl" onClick={handleClick}/>
+        <div ref={addFile}>
+            <button className="hover:bg-gray-500/20 cursor-pointer flex items-center gap-1 rounded-full px-2 py-2" onClick={handleClick}>
+                <span className="icon-[line-md--plus] cursor-pointer text-xl"/>
             </button>
             {isclicked && (<div className="absolute -top-10 left-3 w-52 bg-[#282A2C] backdrop-blur-md rounded-2xl shadow-lg py-2 transition-all duration-300 ease-in-out">
                 <ul>
-                    <li className="text-gray-300 hover:bg-gray-600/30 px-5 py-2 cursor-pointer flex gap-1 items-center">
+                    <li className="text-gray-300 hover:bg-gray-600/30 px-6 py-2 cursor-pointer flex gap-3 items-center">
                         <span className='icon-[mdi--paperclip] text-xl'/>
                         Upload Files
                     </li>
-                    <li className="text-gray-300 hover:bg-gray-600/30 px-5 py-2 cursor-pointer flex gap-1 items-center">
+                    <li className="text-gray-300 hover:bg-gray-600/30 px-6 py-2 cursor-pointer flex gap-3 items-center">
                         <span className='icon-[basil--google-drive-outline] text-xl'/>
                         Add from Drive
                     </li>
                 </ul>
             </div>)}
-        </>
+        </div>
     );
 }
