0f2c53edbe0eaad45e607b1621e6a27370774e66
Preacher-Y <ysheja@gmail.com>
2025-08-03 17:00:56 +0200

    GeminiClone: - Small Tweak

--- GeminiClone/src/components/addFile.tsx
+++ GeminiClone/src/components/addFile.tsx
@@ -8,16 +8,16 @@ export default function AddFile(){
     }
     return (
         <>
-            <div className="hover:bg-gray-500/20 cursor-pointer flex items-center gap-1 rounded-full px-2 py-2">
+            <button className="hover:bg-gray-500/20 cursor-pointer flex items-center gap-1 rounded-full px-2 py-2">
                 <span className="icon-[line-md--plus] cursor-pointer text-xl" onClick={handleClick}/>
-            </div>
-            {isclicked && (<div className="absolute -top-10 left-3 w-48 bg-[#282A2C] backdrop-blur-md rounded-lg shadow-lg py-2 transition-all duration-300 ease-in-out">
+            </button>
+            {isclicked && (<div className="absolute -top-10 left-3 w-52 bg-[#282A2C] backdrop-blur-md rounded-2xl shadow-lg py-2 transition-all duration-300 ease-in-out">
                 <ul>
-                    <li className="text-gray-300 hover:bg-gray-600/30 px-3 py-2 cursor-pointer flex gap-1 items-center">
+                    <li className="text-gray-300 hover:bg-gray-600/30 px-5 py-2 cursor-pointer flex gap-1 items-center">
                         <span className='icon-[mdi--paperclip] text-xl'/>
                         Upload Files
                     </li>
-                    <li className="text-gray-300 hover:bg-gray-600/30 px-3 py-2 cursor-pointer flex gap-1 items-center">
+                    <li className="text-gray-300 hover:bg-gray-600/30 px-5 py-2 cursor-pointer flex gap-1 items-center">
                         <span className='icon-[basil--google-drive-outline] text-xl'/>
                         Add from Drive
                     </li>
