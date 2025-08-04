2183978fbd941ac41386ef74db90037be7dd1695
Preacher-Y <ysheja@gmail.com>
2025-08-04 19:10:44 +0200

    GeminClone: - SideBar.tsx small Tweak

--- GeminiClone/src/components/sideBar.tsx
+++ GeminiClone/src/components/sideBar.tsx
@@ -1,6 +1,7 @@
 import { memo, useState } from "react";
 import clsx from "clsx";
 import { useOpenContext } from "../hooks/openContext";
+import Threedots from "./threeDots";
 function SideBar() {
   const { isOpen, setIsOpen } = useOpenContext();
   const [isHovered, setIsHovered] = useState(false);
@@ -19,40 +20,38 @@ function SideBar() {
 
           <div className="flex items-center justify-between p-4 pb-2 transition-all duration-300 text-gray-100">
             <div className="flex items-center gap-2 text-xl hover:bg-gray-500/20 cursor-pointer rounded-full px-2 py-2">
-               <h1 className="icon-[lucide--menu]"/>
+               <button className="icon-[lucide--menu] cursor-pointer"/>
             </div>
             {isOpen && (
               <div className="flex items-center gap-2 text-xl hover:bg-gray-500/20 cursor-pointer rounded-full px-2 py-2">
-                <h1 className="icon-[ion--search]" />
+                <button className="icon-[ion--search] cursor-pointer" />
               </div>
             )}
           </div>
 
           <div className="text-gray-400/60 px-4 py-2">
             <div className="flex items-center gap-2 font-semibold truncate cursor-pointer transition-all duration-300 ease-in-out rounded-full px-2 py-2">
-              <h1 className="icon-[material-symbols--edit-square-outline-rounded] transition-all duration-300 ease-in-out text-[21px]" />
+              <button className="icon-[material-symbols--edit-square-outline-rounded] cursor-pointer transition-all duration-300 ease-in-out text-[21px]" />
               {isOpen && <span className="transition-all duration-1000 ease-in-out text-md truncate">New Chat</span>}
             </div>
           </div>
           
-          {isOpen &&(<ul className="text-gray-400/70 px-4 transition-all duration-300 ease-in-out truncate">
+          {isOpen &&(<ul className="text-gray-300/70 px-4 transition-all duration-300 ease-in-out truncate">
             <h1 className="px-2 pb-3">Recent</h1>
-            <li className="text-gray-400transition-all flex justify-between relative items-center duration-300 ease-in-out truncate "
+            <li className="text-gray-400 transition-all flex justify-between relative items-center duration-300 ease-in-out truncate "
               onMouseEnter={() => setIsHovered(true)}
               onMouseLeave={() => setIsHovered(false)}
             >
               <div className="py-2 px-4 w-full hover:bg-gray-500/20 cursor-pointer rounded-full">
                 <span>Clone the Gemini</span>
               </div>
-              {isHovered&&(<div className="flex text-gray-200 gap-2 text-lg/1 absolute right-2 hover:bg-gray-500/20 cursor-pointer rounded-full duration-300 transition-all ease-in-out px-2 py-2">
-                <h1 className="icon-[bi--three-dots-vertical]"/>
-              </div>)}
+              {isHovered && (<Threedots />)}
             </li>
           </ul>)}
         </div>
         
         <div className="text-gray-300 mb-4 mx-2 px-4 py-2 flex items-center gap-2 truncate hover:bg-gray-500/20 cursor-pointer rounded-full">
-          <h1 className="icon-[clarity--settings-solid] text-[21px]" />
+          <button className="icon-[clarity--settings-solid] cursor-pointer text-[21px]" />
           {isOpen && <span className="truncate">Settings & Help</span>}
         </div>
       </nav>
---
+++ GeminiClone/src/components/threeDots.tsx
@@ -0,0 +1,24 @@
+import  { memo, useState } from "react";
+
+function Threedots() {
+    const [isClicked,setIsClicked] = useState(false);
+  return (
+    <>
+        <button className="flex text-gray-200 gap-2 text-lg/1 absolute right-2 hover:bg-gray-500/20 cursor-pointer rounded-full duration-300 transition-all ease-in-out px-2 py-2"
+          onClick={() => setIsClicked(!isClicked)}
+        >
+            <h1 className="icon-[bi--three-dots-vertical] cursor-pointer" />
+        </button>
+        {isClicked &&
+        (<div className="bg-[#1F1E20] text-gray-400 absolute right-2 top-10 rounded-lg shadow-lg z-50 p-2">
+            <div>
+                <button className="flex items-center gap-2 hover:bg-gray-500/20 rounded-full px-2 py-1.5 transition-all duration-300 ease-in-out">
+                    <span className="icon-[material-symbols--share] cursor-pointer text-gray-200"/>
+                    <h1 className="text-gray-200 hover:bg-gray-500/20 cursor-pointer rounded-full px-2 py-1.5 transition-all duration-300 ease-in-out">Share</h1>
+                </button>
+            </div>
+        </div>)}
+    </>
+  );
+}
+export default memo(Threedots);
