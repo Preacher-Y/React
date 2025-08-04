09da48f5a5809555370f0dbe6626a2e84ac6883a
Preacher-Y <ysheja@gmail.com>
2025-08-04 16:41:45 +0200

    GeminiClone: - Add truncate on the sidebar

--- GeminiClone/src/components/sideBar.tsx
+++ GeminiClone/src/components/sideBar.tsx
@@ -7,7 +7,7 @@ function SideBar() {
   return (
     <aside
       className={clsx(
-        "h-screen bg-[#282A2C] flex fixed top-0 z-20 flex-col justify-between transition-all duration-300",
+        "h-screen bg-[#282A2C] flex fixed top-0 z-20 flex-col justify-between transition-all ease-in-out duration-300",
         isOpen ? "w-64" : "w-18"
       )}
       onMouseEnter={() => setIsOpen(true)}
@@ -16,7 +16,7 @@ function SideBar() {
       <nav className="h-full flex flex-col justify-between">
         <div className="flex flex-col">
 
-          <div className="flex items-center justify-between p-4 pb-2 text-gray-100">
+          <div className="flex items-center justify-between p-4 pb-2 transition-all duration-300 text-gray-100">
             <div className="flex items-center gap-2 text-xl hover:bg-gray-500/20 cursor-pointer rounded-full px-2 py-2">
                <span className="icon-[lucide--menu]"/>
             </div>
@@ -28,23 +28,23 @@ function SideBar() {
           </div>
 
           <div className="text-gray-400/60 px-4 py-2">
-            <div className="flex items-center gap-2 text-base font-semibold cursor-pointer rounded-full px-2 py-2">
-              <span className="icon-[material-symbols--edit-square-outline-rounded]" />
-              {isOpen && "New Chat"}
+            <div className="flex items-center gap-2 font-semibold truncate cursor-pointer transition-all duration-300 ease-in-out rounded-full px-2 py-2">
+              <h1 className="icon-[material-symbols--edit-square-outline-rounded] transition-all duration-300 ease-in-out text-[21px]" />
+              {isOpen && <span className="transition-all duration-1000 ease-in-out text-md truncate">New Chat</span>}
             </div>
           </div>
           
-          {isOpen &&(<ul className="text-gray-400/70 px-4">
-            <span className="px-2 pb-3">Recent</span>
-            <li className="text-gray-400 px-4 py-2 hover:bg-gray-500/20 cursor-pointer rounded-full">
+          {isOpen &&(<ul className="text-gray-400/70 px-4 transition-all duration-300 ease-in-out truncate">
+            <h1 className="px-2 pb-3">Recent</h1>
+            <li className="text-gray-400 px-4 transition-all duration-300 ease-in-out truncate py-2 hover:bg-gray-500/20 cursor-pointer rounded-full">
               Clone the Gemini 
             </li>
           </ul>)}
         </div>
         
-        <div className="text-gray-300 mb-4 mx-2 px-4 py-2 flex items-center gap-2 hover:bg-gray-500/20 cursor-pointer rounded-full">
-          <span className="icon-[clarity--settings-solid] text-xl" />
-          {isOpen && "Settings & Help"}
+        <div className="text-gray-300 mb-4 mx-2 px-4 py-2 flex items-center gap-2 truncate hover:bg-gray-500/20 cursor-pointer rounded-full">
+          <h1 className="icon-[clarity--settings-solid] text-[21px]" />
+          {isOpen && <span className="truncate">Settings & Help</span>}
         </div>
       </nav>
     </aside>
