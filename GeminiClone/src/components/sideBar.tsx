c7345f508e31f9c3eb2184697762c59f176d37fb
Preacher-Y <ysheja@gmail.com>
2025-08-04 21:45:33 +0200

    GeminiClone: - Small Tweak on the Sidebar

--- GeminiClone/src/components/sideBar.tsx
+++ GeminiClone/src/components/sideBar.tsx
@@ -2,12 +2,15 @@ import { memo, useEffect, useState } from "react";
 import clsx from "clsx";
 import { useOpenContext } from "../hooks/openContext";
 import { useOpen2Context } from "../hooks/open2Context";
+import { useSearchContext } from "../hooks/searchContext";
 import Threedots from "./threeDots";
+
 function SideBar() {
   const { isOpen, setIsOpen } = useOpenContext();
+  const {isSearchClicked,setIsSearchClicked}= useSearchContext();
   const { isOpen2 } = useOpen2Context();
   const [isHovered, setIsHovered] = useState(false);
-  
+
 
   useEffect(()=>{
     if(isOpen2){
@@ -32,8 +35,11 @@ function SideBar() {
                <span className="icon-[lucide--menu] cursor-pointer"/>
             </button>
             {isOpen && (
-              <button className="flex items-center gap-2 text-xl hover:bg-gray-500/20 cursor-pointer rounded-full px-2 py-2">
-                <span className="icon-[ion--search] cursor-pointer" />
+              <button className="flex items-center gap-2 text-xl hover:bg-gray-500/20 cursor-pointer rounded-full px-2 py-2" 
+                onClick={() => setIsSearchClicked(!isSearchClicked)}
+                disabled={isSearchClicked}
+              >
+                <span className={clsx("icon-[ion--search] cursor-pointer",{"text-gray-400/60":isSearchClicked,"":!isSearchClicked})} />
               </button>
             )}
           </div>
@@ -47,7 +53,7 @@ function SideBar() {
           
           {isOpen &&(<ul className="text-gray-300/70 px-4 transition-all duration-300 ease-in-out truncate">
             <h1 className="px-2 pb-3">Recent</h1>
-            <li className="text-gray-400 transition-all flex justify-between relative items-center duration-300 ease-in-out truncate "
+            <li className="text-gray-300 transition-all flex justify-between relative items-center duration-300 ease-in-out truncate "
               onMouseEnter={() => setIsHovered(true)}
               onMouseLeave={() => setIsHovered(false)}
             >
