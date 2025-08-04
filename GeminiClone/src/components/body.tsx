9a43e43ca7d0af9805777acce9f9deef68396b02
Preacher-Y <ysheja@gmail.com>
2025-08-04 21:43:55 +0200

    GeminiClone: - Add the Context to the Body component

--- GeminiClone/src/components/body.tsx
+++ GeminiClone/src/components/body.tsx
@@ -1,5 +1,12 @@
 import { memo } from 'react';
+import { useSearchContext } from '../hooks/searchContext';
+import SearchPage from '../pages/searchPage';
 function Body() {
+  const {isSearchClicked} = useSearchContext();
+
+  if (isSearchClicked) {
+    return <SearchPage />;
+  }
   return (
     <>
         <h1 className="text-center h-screen content-center text-4xl font-bold bg-gradient-to-r from-blue-800 via-blue-400 to-white bg-clip-text text-transparent">
