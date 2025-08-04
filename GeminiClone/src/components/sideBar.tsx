4db734c346861f9c27bd1cc465a0d30fa25893ed
Preacher-Y <ysheja@gmail.com>
2025-08-04 17:07:56 +0200

    GeminiClone: - Sidebar Add the three dots on the lists

--- GeminiClone/src/components/sideBar.tsx
+++ GeminiClone/src/components/sideBar.tsx
@@ -1,8 +1,9 @@
-import { memo } from "react";
+import { memo, useState } from "react";
 import clsx from "clsx";
 import { useOpenContext } from "../hooks/openContext";
 function SideBar() {
   const { isOpen, setIsOpen } = useOpenContext();
+  const [isHovered, setIsHovered] = useState(false);
 
   return (
     <aside
@@ -18,11 +19,11 @@ function SideBar() {
 
           <div className="flex items-center justify-between p-4 pb-2 transition-all duration-300 text-gray-100">
             <div className="flex items-center gap-2 text-xl hover:bg-gray-500/20 cursor-pointer rounded-full px-2 py-2">
-               <span className="icon-[lucide--menu]"/>
+               <h1 className="icon-[lucide--menu]"/>
             </div>
             {isOpen && (
               <div className="flex items-center gap-2 text-xl hover:bg-gray-500/20 cursor-pointer rounded-full px-2 py-2">
-                <span className="icon-[ion--search]" />
+                <h1 className="icon-[ion--search]" />
               </div>
             )}
           </div>
@@ -36,8 +37,16 @@ function SideBar() {
           
           {isOpen &&(<ul className="text-gray-400/70 px-4 transition-all duration-300 ease-in-out truncate">
             <h1 className="px-2 pb-3">Recent</h1>
-            <li className="text-gray-400 px-4 transition-all duration-300 ease-in-out truncate py-2 hover:bg-gray-500/20 cursor-pointer rounded-full">
-              Clone the Gemini 
+            <li className="text-gray-400transition-all flex justify-between relative items-center duration-300 ease-in-out truncate "
+              onMouseEnter={() => setIsHovered(true)}
+              onMouseLeave={() => setIsHovered(false)}
+            >
+              <div className="py-2 px-4 w-full hover:bg-gray-500/20 cursor-pointer rounded-full">
+                <span>Clone the Gemini</span>
+              </div>
+              {isHovered&&(<div className="flex text-gray-200 gap-2 text-lg/1 absolute right-2 hover:bg-gray-500/20 cursor-pointer rounded-full duration-300 transition-all ease-in-out px-2 py-2">
+                <h1 className="icon-[bi--three-dots-vertical]"/>
+              </div>)}
             </li>
           </ul>)}
         </div>
