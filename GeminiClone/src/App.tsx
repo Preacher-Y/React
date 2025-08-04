3ed22574131d0272e8f437cf880873599e00e846
Preacher-Y <ysheja@gmail.com>
2025-08-04 21:41:43 +0200

    GeminiClone: - Added the searchContext Provider to the App

--- GeminiClone/src/App.tsx
+++ GeminiClone/src/App.tsx
@@ -5,12 +5,14 @@ import Header from './components/header';
 import SideBar from './components/sideBar';
 import { openContext } from './hooks/openContext';
 import { open2Context } from './hooks/open2Context';
+import { searchContext } from './hooks/searchContext';
 
 function App() {
   const [isOpen, setIsOpen] = useState(false);
   const [isOpen2, setIsOpen2] = useState(false);
+  const [isSearchClicked, setIsSearchClicked] = useState(false);
   return (
-    <>  
+    <searchContext.Provider value={{ isSearchClicked, setIsSearchClicked }}>  
       <openContext.Provider value={{ isOpen, setIsOpen }}>
       <open2Context.Provider value={{ isOpen2, setIsOpen2 }}>
         <Header/>
@@ -19,7 +21,7 @@ function App() {
       </openContext.Provider>
         <Body/>
         <Chat/>
-    </>
+    </searchContext.Provider>
   );
 }
 
