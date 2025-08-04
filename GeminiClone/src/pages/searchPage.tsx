066da7ce43945dc2b3feadfceb428dc452fe66c3
Preacher-Y <ysheja@gmail.com>
2025-08-04 21:46:21 +0200

    GeminiClone: - Adding some few elements on the search body

--- GeminiClone/src/pages/searchPage.tsx
+++ GeminiClone/src/pages/searchPage.tsx
@@ -1,7 +1,10 @@
+import { memo } from "react";
+
 function SearchPage() {
     return (
-        <main className="max-w-2xl mx-auto min-h-screen">
-            
+        <main className="max-w-2xl mx-auto min-h-screen mt-20">
+            <h1 className="text-2xl text-white font-semibold">Search</h1>
         </main>
     )
 }
+export default memo(SearchPage);
