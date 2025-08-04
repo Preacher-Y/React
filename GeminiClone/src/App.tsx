71b0487881f4b8b5d8ab9e4d038f99b151b4d4ca
Preacher-Y <ysheja@gmail.com>
2025-08-04 13:31:27 +0200

    GeminiClone: - App.tsx Small tweak

--- GeminiClone/src/App.tsx
+++ GeminiClone/src/App.tsx
@@ -1,15 +1,22 @@
+import { memo, useState } from 'react';
 import Body from './components/body';
 import Chat from './components/chat';
 import Header from './components/header';
+import SideBar from './components/sideBar';
+import { openContext } from './hooks/openContext';
 
 function App() {
+  const [isOpen, setIsOpen] = useState(false);
   return (
-    <>
+    <>  
+      <openContext.Provider value={{ isOpen, setIsOpen }}>
         <Header/>
+        <SideBar/>
+      </openContext.Provider>
         <Body/>
         <Chat/>
     </>
   );
 }
 
-export default App;
+export default memo(App);
