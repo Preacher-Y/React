0f5c5a133ed2f34f1d5e7052e8df225164429d13
Preacher-Y <ysheja@gmail.com>
2025-08-07 14:19:47 +0200

    GeminiClone: - Small Tweak

--- GeminiClone/src/components/body.tsx
+++ GeminiClone/src/components/body.tsx
@@ -3,12 +3,13 @@ import { useSearchContext } from '../hooks/searchContext';
 import SearchPage from '../pages/searchPage';
 
 function Body() {
-  
+
   const {isSearchClicked} = useSearchContext();
 
   if (isSearchClicked) {
     return <SearchPage />;
   }
+  
   return (
     <>
         <h1 className="text-center h-screen transition-all duration-300 pb-18 pl-20 ease-in-out content-center text-4xl font-bold bg-gradient-to-r from-blue-800 via-blue-400 to-white bg-clip-text text-transparent">
