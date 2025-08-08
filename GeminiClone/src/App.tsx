354c590a4e22f88c734d63c747d1a39fd1af16df
Preacher-Y <ysheja@gmail.com>
2025-08-08 13:41:02 +0200

    GeminiClone: - Add the Seetings context to teh App

--- GeminiClone/src/App.tsx
+++ GeminiClone/src/App.tsx
@@ -6,14 +6,17 @@ import SideBar from './components/sideBar';
 import { openContext } from './hooks/openContext';
 import { open2Context } from './hooks/open2Context';
 import { searchContext } from './hooks/searchContext';
+import { open3Context } from './hooks/open3Context';
 
 function App() {
   
   const [isOpen, setIsOpen] = useState(false);
   const [isOpen2, setIsOpen2] = useState(false);
+  const [isOpen3, setIsOpen3] = useState(false);
   const [isSearchClicked, setIsSearchClicked] = useState(false);
 
   return (
+    <open3Context.Provider value={{ isOpen3, setIsOpen3 }}>
     <searchContext.Provider value={{ isSearchClicked, setIsSearchClicked }}>  
       <openContext.Provider value={{ isOpen, setIsOpen }}>
       <open2Context.Provider value={{ isOpen2, setIsOpen2 }}>
@@ -24,6 +27,7 @@ function App() {
         <Body/>
         <Chat/>
     </searchContext.Provider>
+    </open3Context.Provider>
   );
 }
 
