bd1bcb9fca91ebe741d5cc6f95a139dfb02a9e75
Preacher-Y <ysheja@gmail.com>
2025-08-09 16:08:53 +0200

    ShoppingMall: - Things getting in the good order.

--- ShoppingMall/src/App.tsx
+++ ShoppingMall/src/App.tsx
@@ -1,12 +1,25 @@
 import Header from './components/header';
+import Landing from './pages/landing';
+
+import React from 'react';
 
 function App() {
+  const [isDisplayed, setIsDisplayed] = React.useState(false);
+
+  React.useEffect(() => {
+    setTimeout(() => {
+      setIsDisplayed(true);
+    }, 5000);
+  }, []);
   return (
     <>
-        <Header/>
-        <div className="text-center mt-20 text-3xl font-bold text-blue-600">
-            Tailwind 4 + Vite
-        </div>
+      {!isDisplayed ? (<Landing/>) :
+        (<>
+          <Header/>
+          <div className="text-center mt-20 text-3xl font-bold text-blue-600">
+              Tailwind 4 + Vite
+          </div>
+        </>)}
     </>
   );
 }
--- ShoppingMall/src/components/header.tsx
+++ ShoppingMall/src/components/header.tsx
@@ -1,7 +1,7 @@
 
 function Header() {
   return (
-    <header className="bg-gray-100 py-4 text-center text-lg font-semibold text-gray-800">
+    <header className="bg-gray-100 dark:bg-[#26272a] py-4 text-center text-lg font-semibold text-gray-800">
       Welcome to ShoppingMall!
     </header>
   );
