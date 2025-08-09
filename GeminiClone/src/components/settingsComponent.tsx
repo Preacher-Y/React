96b6398b0b891db5b4d068170b87586e807fb729
Preacher-Y <ysheja@gmail.com>
2025-08-09 12:40:29 +0200

    GeminiClone: - Small Change

--- GeminiClone/src/components/settingsComponent.tsx
+++ GeminiClone/src/components/settingsComponent.tsx
@@ -1,19 +1,19 @@
 import { useOpenContext } from "../hooks/openContext";
 import { useState } from "react";
-import { useOpen2Context } from "../hooks/open2Context";
-import { useOpen3Context } from "../hooks/open3Context";
+import { useOpenModelContext } from "../hooks/openModelContext";
+import { useOpenSettingsContext } from "../hooks/openSettingsContext";
 import clsx from "clsx";
 
 export default function Settings() {
     const { isOpen } = useOpenContext();
-    const { isOpen2 } = useOpen2Context();
-    const {isOpen3,setIsOpen3} = useOpen3Context();
+    const { isOpenModel } = useOpenModelContext();
+    const {isOpenSettings,setIsOpenSettings} = useOpenSettingsContext();
     const [isSpining, setIsSpinning] = useState(false);
     return (
         <>
             <button className={clsx("text-gray-300 group z-50 absolute bottom-0 mb-4 mx-4 px-2 py-2 flex items-center gap-2 truncate hover:bg-gray-500/20 cursor-pointer rounded-full",{
-            "w-[88%]": isOpen&&!isOpen2,
-            "":isOpen2&&!isOpen,
+            "w-[88%]": isOpen&&!isOpenModel,
+            "":isOpenModel&&!isOpen,
             })}
                 onMouseEnter={() =>{ 
                     setIsSpinning(true)
@@ -25,14 +25,14 @@ export default function Settings() {
                     setTimeout(() => {
                      setIsSpinning(false)
                 }, 300)}}
-                onClick={()=>setIsOpen3(prev=>!prev)}
+                onClick={()=>setIsOpenSettings(prev=>!prev)}
             >
                 <span className={clsx("icon-[clarity--settings-solid] cursor-pointer text-[21px]",{
                     "animate-spin": isSpining,
                 })} />
-                {!isOpen2 && isOpen && <span className="truncate">Settings & Help</span>}
+                {!isOpenModel && isOpen && <span className="truncate">Settings & Help</span>}
             </button>
-            {isOpen3 && (
+            {isOpenSettings && (
                 <div className="absolute bottom-10 left-32 z-50 w-52 bg-[#282A2C] backdrop-blur-md rounded-2xl shadow-lg py-2 transition-all duration-300 ease-in-out">
                     <div>
                         <button className="flex items-center gap-2 hover:bg-gray-500/20 rounded-full px-2 py-1.5 transition-all duration-300 ease-in-out">
