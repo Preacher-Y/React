4dd38f643a5dc6822fd0fdcf71de7295bf9a35e0
Preacher-Y <ysheja@gmail.com>
2025-08-09 20:39:58 +0200

    ShoppingMall: - Add responsiveness for the laptop view

--- ShoppingMall/src/components/header.tsx
+++ ShoppingMall/src/components/header.tsx
@@ -4,7 +4,7 @@ import Words from '../assets/android-chrome-512x512.png'
 
 function Header() {
   return (
-    <header className="flex justify-between items-center px-32 text-center text-lg font-semibold text-gray-800">
+    <header className="flex z-50 pt-4 justify-between items-center px-32 text-center text-lg font-semibold text-gray-800">
       <div className='flex items-center'>
         <motion.img 
             src={Logo} 
@@ -24,41 +24,41 @@ function Header() {
             transition={{ duration: 0.5, delay: 0.17 , ease: "easeIn" }}
         />
       </div>
-      <nav className='flex gap-6 items-center mt-4'>
+      <nav className='flex gap-8 items-center mt-4'>
         <div className='group relative'>
-          <button className='group-hover:text-[#ED573E] group-hover:-translate-y-1.5 transition-all duration-300'>
+          <button className='group-hover:text-[#ED573E] max-lg:group-hover:text-white group-hover:-translate-y-1.5 transition-all duration-300'>
             Home
           </button>
-          <div className='absolute -bottom-2 left-1/2 transform -translate-x-1/2 w-1.5 h-1.5 rounded-full bg-[#ED573E] opacity-0 group-hover:opacity-100 transition-all duration-300'></div>
+          <div className='absolute -bottom-2 left-1/2 transform -translate-x-1/2 w-1.5 h-1.5 rounded-full bg-[#ED573E] max-lg:bg-white opacity-0 group-hover:opacity-100 transition-all duration-300'></div>
         </div>
 
         <div className='group relative'>
-          <button className='group-hover:text-[#ED573E] group-hover:-translate-y-1.5 transition-all duration-300'>
+          <button className='group-hover:text-[#ED573E] max-lg:group-hover:text-white group-hover:-translate-y-1.5 transition-all duration-300'>
             About
           </button>
-          <div className='absolute -bottom-2 left-1/2 transform -translate-x-1/2 w-1.5 h-1.5 rounded-full bg-[#ED573E] opacity-0 group-hover:opacity-100 transition-all duration-300'></div>
+          <div className='absolute -bottom-2 left-1/2 transform -translate-x-1/2 w-1.5 h-1.5 rounded-full bg-[#ED573E] max-lg:bg-white opacity-0 group-hover:opacity-100 transition-all duration-300'></div>
         </div>
 
         <div className='group relative'>
-          <button className='group-hover:text-[#ED573E] group-hover:-translate-y-1.5 transition-all duration-300'>
+          <button className='group-hover:text-white group-hover:-translate-y-1.5 transition-all duration-300'>
             Testimonials
           </button>
-          <div className='absolute -bottom-2 left-1/2 transform -translate-x-1/2 w-1.5 h-1.5 rounded-full bg-[#ED573E] opacity-0 group-hover:opacity-100 transition-all duration-300'></div>
+          <div className='absolute -bottom-2 left-1/2 transform -translate-x-1/2 w-1.5 h-1.5 rounded-full bg-white opacity-0 group-hover:opacity-100 transition-all duration-300'></div>
         </div>
 
         <div className='group relative'>
-          <button className='group-hover:text-[#ED573E] group-hover:-translate-y-1.5 transition-all duration-300'>
+          <button className='group-hover:text-white group-hover:-translate-y-1.5 transition-all duration-300'>
             Contact
           </button>
-          <div className='absolute -bottom-2 left-1/2 transform -translate-x-1/2 w-1.5 h-1.5 rounded-full bg-[#ED573E] opacity-0 group-hover:opacity-100 transition-all duration-300'></div>
+          <div className='absolute -bottom-2 left-1/2 transform -translate-x-1/2 w-1.5 h-1.5 rounded-full bg-white opacity-0 group-hover:opacity-100 transition-all duration-300'></div>
         </div>
 
         <button className='items-center flex'>
-          <h1 className='icon-[mdi--cart-outline] text-[22px] hover:text-[#ED573E]'/>
+          <h1 className='icon-[mdi--cart-outline] text-[22px] hover:text-white'/>
         </button>
         
-        <button className='bg-white px-4 py-2 rounded-full hover:bg-white/70 transition-colors duration-300'>
-          Booking Now
+        <button className='bg-white px-4 py-2 rounded-full hover:bg-transparent hover:text-white hover:ring-2 transition-all duration-700 ease-in-out'>
+          Order Now
         </button>
       </nav>
     </header>
