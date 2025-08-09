2a01f1073a668abdc53a7393d33cc71fdba2d89a
Preacher-Y <ysheja@gmail.com>
2025-08-09 12:40:46 +0200

    GeminiClone: - Small Change

--- GeminiClone/src/components/threeDots.tsx
+++ GeminiClone/src/components/threeDots.tsx
@@ -1,17 +1,17 @@
-import  { memo, useState } from "react";
+import  { memo } from "react";
+import { useOpenThreeDotsContext } from "../hooks/openThreeDotsContext";
 
 function Threedots() {
-
-  const [isClicked,setIsClicked] = useState(false);
+  const {isThreeDotsClicked,setIsThreeDotsClicked} = useOpenThreeDotsContext();
   
   return (
     <>
         <button className="flex text-gray-200 gap-2 text-lg/1 hover:bg-gray-500/20 cursor-pointer rounded-full duration-300 transition-all ease-in-out px-2 py-2"
-          onClick={() => setIsClicked(!isClicked)}
+          onClick={() => setIsThreeDotsClicked(!isThreeDotsClicked)}
         >
             <h1 className="icon-[bi--three-dots-vertical] cursor-pointer" />
         </button>
-        {isClicked &&
+        {isThreeDotsClicked &&
         (<div className="bg-[#1F1E20] text-gray-400 absolute right-2 top-10 rounded-lg shadow-lg z-50 p-2">
             <div>
                 <button className="flex items-center gap-2 hover:bg-gray-500/20 rounded-full px-2 py-1.5 transition-all duration-300 ease-in-out">
