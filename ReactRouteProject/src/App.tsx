7922bd6ec51c275370f50dcd0fbe423c51f343a4
Preacher-Y <ysheja@gmail.com>
2025-08-11 10:54:52 +0200

    React Router Project: - Innitial Commit

---
+++ ReactRouteProject/src/App.tsx
@@ -0,0 +1,14 @@
+import Header from './components/header';
+
+function App() {
+  return (
+    <>
+        <Header/>
+        <div className="text-center mt-20 text-3xl font-bold text-blue-600">
+            Tailwind 4 + Vite
+        </div>
+    </>
+  );
+}
+
+export default App;
