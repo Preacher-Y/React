3964e6055aabeded9d8220b46cd5961bff22c9f7
Preacher-Y <ysheja@gmail.com>
2025-08-11 19:52:16 +0200

    React Router Project - Done with the Home Page

--- ReactRouteProject/src/App.tsx
+++ ReactRouteProject/src/App.tsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Header from './components/header';
+import Home from './pages/home';
 
 function App() {
   return (
     <>
       <BrowserRouter>
+        <Header/>
         <Routes>
-          <Route path="/" element={<Header />} />
+          <Route path="/" element={<Home />} />
         </Routes>
       </BrowserRouter>
     </>
---
+++ ReactRouteProject/src/pages/home.tsx
@@ -0,0 +1,22 @@
+import { memo } from "react";
+import BgHero from "../assets/home-hero.png" 
+import { Link } from "react-router-dom";
+
+function Home(){
+    return(
+
+        <div
+            style={{ backgroundImage: `url(${BgHero})` }}
+            className="bg-cover bg-center h-[92vh] flex flex-col items-center gap-6 justify-center px-6"
+        >
+            <h1 className="text-3xl text-white font-bold">You got the travel plans, we got the travel vans.</h1>
+            <p className="text-gray-200 font-medium ">Add adventure to your life by joining the #vanlife movement. Rent the perfect van to make your perfect road trip.</p>
+            <Link
+                to="/van"
+                className="bg-[#FF8C38] py-3 w-full text-lg text-white font-semibold tracking-wider block text-center"
+                >Find your van</Link>
+        </div>
+
+    )
+}
+export default memo(Home)
