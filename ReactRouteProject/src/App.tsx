e9cae5015018935744a929f323f20e48d6432a43
Preacher-Y <ysheja@gmail.com>
2025-08-13 19:53:09 +0200

    React Router Project: - Small adjustment

--- ReactRouteProject/src/App.tsx
+++ ReactRouteProject/src/App.tsx
@@ -1,15 +1,14 @@
-import Header from './components/header';
 import Home from './pages/home';
+import Vans from './pages/vans';
 import About from './pages/about';
-import Footer from './components/footer';
+import DetailsVan from './pages/detailsVan';
 
 import type { VanType } from './type';
 
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { useState } from 'react';
 import "./server"
-import Vans from './pages/vans';
-import DetailsVan from './pages/detailsVan';
+import Layout from './components/Layout';
 
 
 function App() {
@@ -18,14 +17,14 @@ function App() {
   return (
     <>
       <BrowserRouter>
-        <Header/>
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/vans" element={<Vans data={data} setData={setData}/>} />
-          <Route path="/vans/:name" element={<DetailsVan data={data}/>} />
+          <Route element={<Layout/>}>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/vans" element={<Vans data={data} setData={setData}/>} />
+            <Route path="/vans/:name" element={<DetailsVan data={data}/>} />
+          </Route>
         </Routes>
-        <Footer/>
       </BrowserRouter>
     </>
   );
