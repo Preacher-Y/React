6e04193d2e25dfc41e0e774e983677e023765184
Preacher-Y <ysheja@gmail.com>
2025-08-13 19:14:33 +0200

    React Router Project: - Small adjustment

--- ReactRouteProject/src/pages/home.tsx
+++ ReactRouteProject/src/pages/home.tsx
@@ -7,12 +7,12 @@ function Home(){
 
         <div
             style={{ backgroundImage: `url(${BgHero})` }}
-            className="bg-cover bg-center h-[92vh] flex flex-col items-center gap-6 justify-center px-6"
+            className="bg-cover bg-center h-screen flex flex-col items-center gap-6 justify-center px-6"
         >
             <h1 className="text-3xl text-white font-bold">You got the travel plans, we got the travel vans.</h1>
             <p className="text-gray-200 font-medium ">Add adventure to your life by joining the #vanlife movement. Rent the perfect van to make your perfect road trip.</p>
             <Link
-                to="/van"
+                to="/vans"
                 className="bg-[#FF8C38] py-3 w-full text-lg text-white font-semibold tracking-wider block text-center"
             >Find your van</Link>
         </div>
