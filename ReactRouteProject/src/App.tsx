a41657e31f4bc42696c1d12d4fad172b04206cdb
Preacher-Y <ysheja@gmail.com>
2025-08-13 19:14:15 +0200

    React Router Project: - Small adjustment

--- ReactRouteProject/src/App.tsx
+++ ReactRouteProject/src/App.tsx
@@ -3,12 +3,18 @@ import Home from './pages/home';
 import About from './pages/about';
 import Footer from './components/footer';
 
+import type { VanType } from './type';
+
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { useState } from 'react';
 import "./server"
 import Vans from './pages/vans';
 import DetailsVan from './pages/detailsVan';
 
+
 function App() {
+  const [data, setData]=useState<VanType>([])
+
   return (
     <>
       <BrowserRouter>
@@ -16,8 +22,8 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
-          <Route path="/vans" element={<Vans/>} />
-          <Route path="/vans/:id" element={<DetailsVan/>} />
+          <Route path="/vans" element={<Vans data={data} setData={setData}/>} />
+          <Route path="/vans/:name" element={<DetailsVan data={data}/>} />
         </Routes>
         <Footer/>
       </BrowserRouter>
