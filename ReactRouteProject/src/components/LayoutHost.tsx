3645576a3f13746ec80b0fee23f318c7a41bc782
Preacher-Y <ysheja@gmail.com>
2025-08-14 13:31:04 +0200

    React Router Project: - Small adjustment

--- ReactRouteProject/src/components/LayoutHost.tsx
+++ ReactRouteProject/src/components/LayoutHost.tsx
@@ -4,15 +4,16 @@ import { memo } from "react";
 
 function LayoutHost(){
 
-    const normal = "text-gray-600 underline-offset-3 text-sm hover:text-gray-950 hover:underline"
+    const normal = "text-gray-600 underline-offset-3 font-semibold text-sm hover:text-gray-950 hover:underline"
     const active = "text-gray-950 underline"
 
 
     return(
         <>
-            <nav className="flex gap-6 my-8">
+            <nav className="flex px-6 gap-6 my-8">
                 <NavLink to="" end className={({isActive})=>clsx(normal,isActive&&active)}>Dashboard</NavLink>
                 <NavLink to="income" className={({isActive})=>clsx(normal,isActive&&active)}>Income</NavLink>
+                <NavLink to="vans" className={({isActive})=>clsx(normal,isActive&&active)}>Vans</NavLink>
                 <NavLink to="reviews" className={({isActive})=>clsx(normal,isActive&&active)}>Reviews</NavLink>
             </nav>
             <main>
