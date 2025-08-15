491cb980d7121c24cab2b15ea55881e2e38786e0
Preacher-Y <ysheja@gmail.com>
2025-08-15 22:13:22 +0200

    React Router Project: - Small Adjustment

--- ReactRouteProject/src/pages/Hosts/vans.tsx
+++ ReactRouteProject/src/pages/Hosts/vans.tsx
@@ -1,4 +1,5 @@
 import { memo } from "react"
+import { Link } from "react-router-dom"
 import type { VanType } from "../../type"
 
 function Vans({data}:{data:VanType}){
@@ -6,8 +7,8 @@ function Vans({data}:{data:VanType}){
         <div className="px-6 max-md:h-[76.5vh] mt-8 mb-16">
             <h1 className="font-bold text-3xl mb-4">Your listed vans</h1>
             <div className=" flex flex-col gap-5  py-2 overflow-y-scroll [&::-webkit-scrollbar]:hidden [-ms-overflow-style:'none'] [scrollbar-width:'none'] ">
-                    {data.map(el=>
-                    <article className="flex justify-between rounded-md hover:shadow-xl items-center pr-3 mx-4 bg-white">
+                {data.map(el=>
+                    <Link to={`${el.name.split(' ').join('')}`} key={el.id} className="flex justify-between rounded-md hover:shadow-xl items-center pr-3 mx-4 bg-white">
                         <div className="flex gap-3">
                             <img src={el.imageUrl} alt="" className="w-20 h-20 rounded-md "/>
                             <div className="grid py-2">
@@ -15,8 +16,8 @@ function Vans({data}:{data:VanType}){
                                 <p className="text-gray-700">{`$${el.price.toLocaleString()}/day`}</p>
                             </div>
                         </div>
-                    </article>)}
-                </div>
+                    </Link>)}
+            </div>
         </div>
     )
 }
