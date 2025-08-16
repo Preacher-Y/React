93412da26ab262a7395ad5d002467e6fce49ae8f
Preacher-Y <ysheja@gmail.com>
2025-08-16 16:57:15 +0200

    React Router Project: - Small Adjustment

--- ReactRouteProject/src/pages/login.tsx
+++ ReactRouteProject/src/pages/login.tsx
@@ -1,7 +1,12 @@
 function Login(){
     return(
-        <section className="h-full px-6 grid justify-center w-full text-center mt-14 mb-16">
+        <section className="h-full px-6 grid w-full text-center mt-14 mb-16">
             <h1 className="font-bold text-2xl">Sign in to your account</h1>
+
+            <form className="flex flex-col gap-2 mx-6">
+                <input type="email" placeholder="Email" required className="border border-gray-300 rounded-md px-2 py-1"/>
+                <input type="password" placeholder="Password"  required className="border border-gray-300 rounded-md px-2 py-1"/>
+            </form>
         </section>
     )
 }
