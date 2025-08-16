db3bbe0d0f2eceb45aff2172fe670d707b21735c
Preacher-Y <ysheja@gmail.com>
2025-08-16 09:33:29 +0200

    React Router Project: - Small Adjustment

--- ReactRouteProject/src/pages/login.tsx
+++ ReactRouteProject/src/pages/login.tsx
@@ -1,7 +1,8 @@
 function Login(){
     return(
-        <>
-        </>
+        <section className="h-full px-6 grid justify-center w-full text-center mt-14 mb-16">
+            <h1 className="font-bold text-2xl">Sign in to your account</h1>
+        </section>
     )
 }
 
