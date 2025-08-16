ba97d61bdcb45ab90ee76ccc4807282d00452297
Preacher-Y <ysheja@gmail.com>
2025-08-16 17:05:13 +0200

    React Router Project: - Small Adjustment

--- ReactRouteProject/src/pages/login.tsx
+++ ReactRouteProject/src/pages/login.tsx
@@ -1,12 +1,17 @@
+
+import { Link } from "react-router-dom"
+
 function Login(){
     return(
         <section className="h-full px-6 grid w-full text-center mt-14 mb-16">
             <h1 className="font-bold text-2xl">Sign in to your account</h1>
 
-            <form className="flex flex-col gap-2 mx-6">
-                <input type="email" placeholder="Email" required className="border border-gray-300 rounded-md px-2 py-1"/>
-                <input type="password" placeholder="Password"  required className="border border-gray-300 rounded-md px-2 py-1"/>
+            <form className="flex flex-col gap-2 mx-6 mt-8">
+                <input type="email" placeholder="Email" required className="border border-gray-300 rounded-md px-3 py-2"/>
+                <input type="password" placeholder="Password"  required className="border border-gray-300 rounded-md px-3 py-2"/>
+                <button className="bg-[#FF8C38] text-white py-2 rounded-md mt-6">Sign In</button>
             </form>
+            <Link to='SignUp' className='cursor-pointer'>Don’t have an account? <span className="text-[#FF8C38]">Create one now</span></Link>
         </section>
     )
 }
