253008c3673bae8e3f98ff6d2a23cdbb45eedbec
Preacher-Y <ysheja@gmail.com>
2025-08-16 17:07:09 +0200

    React Router Project: - Small Adjustment

--- ReactRouteProject/src/pages/login.tsx
+++ ReactRouteProject/src/pages/login.tsx
@@ -11,7 +11,7 @@ function Login(){
                 <input type="password" placeholder="Password"  required className="border border-gray-300 rounded-md px-3 py-2"/>
                 <button className="bg-[#FF8C38] text-white py-2 rounded-md mt-6">Sign In</button>
             </form>
-            <Link to='SignUp' className='cursor-pointer'>Don’t have an account? <span className="text-[#FF8C38]">Create one now</span></Link>
+            <Link to='/SignUp' className='cursor-pointer text-sm mt-4'>Don’t have an account? <span className="text-[#FF8C38] hover:text-[#d23a10]">Create one now</span></Link>
         </section>
     )
 }
