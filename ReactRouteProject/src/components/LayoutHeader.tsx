35f9dc5a47f525431c742bbe880e9af5253d8d39
Preacher-Y <ysheja@gmail.com>
2025-08-18 12:36:10 +0200

    React Router Project: - Small Adjustment

--- ReactRouteProject/src/components/LayoutHeader.tsx
+++ ReactRouteProject/src/components/LayoutHeader.tsx
@@ -2,16 +2,22 @@ import Header from "./header"
 import Footer from "./footer"
 
 import { memo } from "react"
-import { Outlet } from "react-router-dom"
+import { Outlet,useNavigation } from "react-router-dom"
 
 function Layout() {
+
+    const nav = useNavigation()
+
     return (
         <div className="flex flex-col min-h-screen">
 
             <Header />
 
             <main className="flex-grow">
-                <Outlet />
+                {nav.state !== 'idle'?(<div className="grid place-items-center text-center mt-32 h-64">
+                    <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-orange-500 mx-auto"/>
+                    <p className="animate-pulse -mt-32 ml-2">Loading ...</p>
+                </div>): <Outlet />}
             </main>
 
             <Footer />
