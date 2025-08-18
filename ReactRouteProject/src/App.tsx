978cb52b1c6f64cfeb9ecca44916255d5db43057
Preacher-Y <ysheja@gmail.com>
2025-08-18 12:12:45 +0200

    React Router Project: - Small Adjustment

--- ReactRouteProject/src/App.tsx
+++ ReactRouteProject/src/App.tsx
@@ -42,7 +42,7 @@ function Loader(){
       return json.vans
 
   })()
-  
+
 }
 
 
@@ -55,17 +55,17 @@ function App() {
             <Route index element={<Home />} />
             <Route path="about" element={<About />} />
             <Route path="vans" element={<Vans />}loader={Loader} />
-            <Route path="vans/:name" element={<DetailsVan/>} errorElement={<FetchErrors/>} loader={Loader} />
+            <Route path="vans/:name" element={<DetailsVan/>} loader={Loader} />
             <Route path="SignIn" element={<Login/>}/>
             <Route path="SignUp" element={<SignUp/>}/>
             
             <Route path="host" element={<LayoutHost/>} >
-              <Route index element={<Dashboard/>} errorElement={<FetchErrors/>} loader={Loader}/>
+              <Route index element={<Dashboard/>} loader={Loader}/>
               <Route path="reviews" element={<Reviews/>} />
               <Route path="income" element={<Income/>} />
-              <Route path="vans" element={<VansHost/>} errorElement={<FetchErrors/>} loader={Loader} />
+              <Route path="vans" element={<VansHost/>} loader={Loader} />
 
-              <Route path="vans/:name" element={<HostVanDetails/>} errorElement={<FetchErrors/>} loader={Loader}>
+              <Route path="vans/:name" element={<HostVanDetails/>} loader={Loader}>
                 <Route index element={<Details/>}/>
                 <Route path="pricing" element={<Pricing/>}/>
                 <Route path="Photos" element={<Pic/>}/>
