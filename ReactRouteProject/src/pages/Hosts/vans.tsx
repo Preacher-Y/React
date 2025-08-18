726a19c10743a8169c4a1f0879fc1687d868aea8
Preacher-Y <ysheja@gmail.com>
2025-08-18 11:50:10 +0200

    React Router Project: - Small Adjustment

--- ReactRouteProject/src/pages/Hosts/vans.tsx
+++ ReactRouteProject/src/pages/Hosts/vans.tsx
@@ -8,25 +8,19 @@ function Vans(){
 
     return(
         <div className="px-6 max-md:h-[76.5vh] mt-8 mb-16">
-           {data.length==0?(<div className="grid place-items-center text-center h-64">
-                 <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-orange-500 mx-auto"></div>
-                 <p className="animate-pulse -mt-24">Loading ...</p>
-            </div>):( 
-            <>
-                <h1 className="font-bold text-3xl mb-4">Your listed vans</h1>
-                <div className=" flex flex-col gap-5  py-2 overflow-y-scroll [&::-webkit-scrollbar]:hidden [-ms-overflow-style:'none'] [scrollbar-width:'none'] ">
-                    {data.map(el=>
-                        <Link to={`${el.name.split(' ').join('')}`} key={el.id} className="flex justify-between rounded-md hover:shadow-xl items-center pr-3 mx-4 bg-white">
-                            <div className="flex gap-3">
-                                <img src={el.imageUrl} alt="" className="w-20 h-20 rounded-md "/>
-                                <div className="grid py-2">
-                                    <h1 className="font-semibold">{el.name}</h1>
-                                    <p className="text-gray-700">{`$${el.price.toLocaleString()}/day`}</p>
-                                </div>
+            <h1 className="font-bold text-3xl mb-4">Your listed vans</h1>
+            <div className=" flex flex-col gap-5  py-2 overflow-y-scroll [&::-webkit-scrollbar]:hidden [-ms-overflow-style:'none'] [scrollbar-width:'none'] ">
+                {data.map(el=>
+                    <Link to={`${el.name.split(' ').join('')}`} key={el.id} className="flex justify-between rounded-md hover:shadow-xl items-center pr-3 mx-4 bg-white">
+                        <div className="flex gap-3">
+                            <img src={el.imageUrl} alt="" className="w-20 h-20 rounded-md "/>
+                            <div className="grid py-2">
+                                <h1 className="font-semibold">{el.name}</h1>
+                                <p className="text-gray-700">{`$${el.price.toLocaleString()}/day`}</p>
                             </div>
-                        </Link>)}
-                </div>
-           </>)}
+                        </div>
+                     </Link>)}
+            </div>
         </div>
     )
 }
