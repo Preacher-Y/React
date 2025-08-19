9dd86003e448c4c93516b45f1a104b963f9a26c7
Preacher-Y <ysheja@gmail.com>
2025-08-19 20:45:03 +0200

    React Router Project: - Small Adjustment

--- ReactRouteProject/src/pages/login.tsx
+++ ReactRouteProject/src/pages/login.tsx
@@ -30,6 +30,7 @@ function Login(){
 
 
     return(
+        
         <section className="h-full px-6 grid w-full text-center mt-14 mb-16">
             <ToastContainer autoClose={2000} position="top-center"/>
             <h1 className="font-bold text-2xl">Sign in to your account</h1>
