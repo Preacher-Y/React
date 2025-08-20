262fcc96a7ec921bc3e6fa2dee7c2bacf665e4d1
Preacher-Y <ysheja@gmail.com>
2025-08-20 18:49:34 +0200

    React Router Project: - Small Adjustment

--- ReactRouteProject/src/pages/Hosts/vans.tsx
+++ ReactRouteProject/src/pages/Hosts/vans.tsx
@@ -1,18 +1,20 @@
 import { memo } from "react"
-import { Link,useLoaderData } from "react-router-dom"
+import { Link,useLoaderData, useLocation } from "react-router-dom"
 
 import type { VanType } from "../../type"
 
 function Vans(){
 
     const data:VanType = useLoaderData()
+    const location = useLocation()
+    const id = location.state?.id || '';
 
     return(
 
         <div className="px-6 max-md:h-[76.5vh] mt-8 mb-16">
             <h1 className="font-bold text-3xl mb-4">Your listed vans</h1>
             <div className=" flex flex-col gap-5  py-2 overflow-y-scroll [&::-webkit-scrollbar]:hidden [-ms-overflow-style:'none'] [scrollbar-width:'none'] ">
-                {data.map(el=>
+                {data.filter((el) => el.hostId === id).map(el=>
                     <Link to={`${el.name.split(' ').join('')}`} key={el.id} className="flex justify-between rounded-md hover:shadow-xl items-center pr-3 mx-4 bg-white">
                         <div className="flex gap-3">
                             <img src={el.imageUrl} alt="" className="w-20 h-20 rounded-md "/>
