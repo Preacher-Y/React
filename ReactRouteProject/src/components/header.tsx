9d6019f27ec84e83554e9b0ac939ff914717f19e
Preacher-Y <ysheja@gmail.com>
2025-08-21 20:23:01 +0200

    React Router Project: - Small Adjustment

--- ReactRouteProject/src/components/header.tsx
+++ ReactRouteProject/src/components/header.tsx
@@ -5,6 +5,7 @@ import { useEffect, useState } from 'react';
 
 function Header() {
   const [isLoggedIn, setIsLoggedIn] = useState(localStorage.getItem("loggedIn") === 'true')
+  const [show,setShow] = useState(false)
   const navigate = useNavigate()
 
   useEffect(() => {
@@ -23,6 +24,8 @@ function Header() {
 
   function logOut() {
     localStorage.removeItem("loggedIn")
+    localStorage.removeItem("id")
+    localStorage.removeItem("img")
     setIsLoggedIn(false)
     window.dispatchEvent(new Event('authStateChanged'))
     navigate('/SignIn', { state: { message: "You have been Logged Out !" }, replace: true })
@@ -30,6 +33,7 @@ function Header() {
 
   const normal = "text-gray-600 underline-offset-3 text-sm hover:text-gray-950 hover:underline"
   const active = "text-gray-950 underline"
+  const img = localStorage.getItem('img')||""
 
   return (
     <header className="bg-gray-50 py-4 text-center text-lg shadow-[0px_1px_0px_gray]/30 font-semibold text-gray-800">
@@ -57,9 +61,12 @@ function Header() {
                 <NavLink to="/SignIn" className="icon-[solar--user-circle-outline] text-xl text-gray-600 hover:text-gray-900 hover:scale-110 transition-all duration-300" />
               </li>
             ) : (
-              <li onClick={logOut} className='flex cursor-pointer gap-1 items-center text-sm group text-gray-600 hover:text-gray-900 hover:scale-105 transition-all duration-300'>
-                <h1 className='icon-[hugeicons--logout-04] group-hover:text-red-600' />
-                <p>logout</p>
+              <li className='flex cursor-pointer overflow-hidden items-center text-sm group text-gray-600 hover:text-gray-900 transition-all duration-300'>
+                <img onClick={()=>setShow(prev=>!prev)} src={img} alt="profilePic" className='w-10 h-10 rounded-full object-cover object-center' />
+                {show&&(<div onClick={logOut} className='flex items-center gap-1.5 shadow-[0px_2px_2px] z-10 absolute px-3 py-2 top-15 left-9/14 rounded-md bg-amber-50 hover:bg-gray-300'>
+                  <h1 className='icon-[hugeicons--logout-04] group-hover:text-red-600' />
+                  <p>logout</p>
+                </div>)}
               </li>
             )}
           </ul>
