1bee2b5382af41836f645472960643c22a9b15b7
Preacher-Y <ysheja@gmail.com>
2025-08-21 18:14:02 +0200

    React Router Project: - Small Adjustment

--- ReactRouteProject/src/components/header.tsx
+++ ReactRouteProject/src/components/header.tsx
@@ -33,9 +33,12 @@ function Header() {
               <NavLink to="/vans" className={({isActive})=>clsx(normal,isActive&&active)}>Vans</NavLink>
             </li>
 
-            <li id='user' aria-label='user' className='flex'>
+            {!localStorage.getItem("loggedIn")?(<li id='user' aria-label='user' className='flex'>
               <NavLink to="/SignIn" className="icon-[solar--user-circle-outline] text-xl text-gray-600 hover:text-gray-900 hover:scale-110 transition-all duration-300"/>
-            </li>
+            </li>):(<li onClick={()=>localStorage.removeItem('loggedIn')} className='flex cursor-pointer gap-1 items-center text-sm group text-gray-600 hover:text-gray-900 hover:scale-105 transition-all duration-300'>
+              <h1 className='icon-[hugeicons--logout-04] group-hover:text-red-600'/>
+              <p className='mt-0.5'>logout</p> 
+            </li>)}
 
           </ul>
 
