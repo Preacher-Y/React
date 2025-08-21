bd4a57dc9d793ed03baf1e5818d8d950db3f2bc4
Preacher-Y <ysheja@gmail.com>
2025-08-21 18:29:59 +0200

    React Router Project: - Small Adjustment

--- ReactRouteProject/src/components/header.tsx
+++ ReactRouteProject/src/components/header.tsx
@@ -1,9 +1,29 @@
 import Logo from '../assets/VanLogo.svg'
-import { NavLink, Link } from 'react-router-dom';
+import { NavLink, Link, useNavigate } from 'react-router-dom';
 import clsx from 'clsx';
+import { useEffect, useState } from 'react';
 
 
 function Header() {
+
+  const [isLoggedIn, setIsLoggedIn]=useState(localStorage.getItem("loggedIn")==='true')
+
+  const navigate = useNavigate()
+
+  useEffect(()=>{
+    const storageChange= ()=>{
+      setIsLoggedIn(localStorage.getItem("loggedIn")==='true');
+    }
+    window.addEventListener('storage',storageChange)
+
+    return ()=> window.removeEventListener('storage',storageChange)
+  },[])
+
+  function logOut(){
+    localStorage.removeItem("loggedIn")
+    setIsLoggedIn(false)
+    navigate('/SignIn',{state:{message:"You have been Logged Out !"},replace:true})
+  }
 
   const normal = "text-gray-600 underline-offset-3 text-sm hover:text-gray-950 hover:underline"
   const active = "text-gray-950 underline"
@@ -33,9 +53,9 @@ function Header() {
               <NavLink to="/vans" className={({isActive})=>clsx(normal,isActive&&active)}>Vans</NavLink>
             </li>
 
-            {!localStorage.getItem("loggedIn")?(<li id='user' aria-label='user' className='flex'>
+            {!isLoggedIn?(<li id='user' aria-label='user' className='flex'>
               <NavLink to="/SignIn" className="icon-[solar--user-circle-outline] text-xl text-gray-600 hover:text-gray-900 hover:scale-110 transition-all duration-300"/>
-            </li>):(<li onClick={()=>localStorage.removeItem('loggedIn')} className='flex cursor-pointer gap-1 items-center text-sm group text-gray-600 hover:text-gray-900 hover:scale-105 transition-all duration-300'>
+            </li>):(<li onClick={logOut} className='flex cursor-pointer gap-1 items-center text-sm group text-gray-600 hover:text-gray-900 hover:scale-105 transition-all duration-300'>
               <h1 className='icon-[hugeicons--logout-04] group-hover:text-red-600'/>
               <p className='mt-0.5'>logout</p> 
             </li>)}
