7d35fcae9448e82c94bb0ecbb92c434a260bb174
Preacher-Y <ysheja@gmail.com>
2025-08-23 15:25:14 +0200

    ShoppingMall: - Small adjustment

---
+++ ShoppingMall/src/components/reachout.tsx
@@ -0,0 +1,33 @@
+import { useRef } from 'react';
+import { motion, useInView } from 'framer-motion';
+
+function ReachOut(){
+    const ref = useRef(null);
+    const isInView = useInView(ref, { once: false, amount: 0.3 });
+    return(
+        <section className="px-32 py-20 mb-10 text-center" ref={ref}>
+            <motion.div
+                initial={{ opacity: 0, y: 20 }}
+                animate={isInView ? { opacity: 1, y: 0 } : { opacity: 0, y: 20 }}
+                transition={{ duration: 0.5, delay: 0.2 }}
+                className='max-w-3xl mx-auto'>
+
+                <motion.h1
+                    initial={{ opacity: 0, y: 20 }}
+                    animate={isInView ? { opacity: 1, y: 0 } : { opacity: 0, y: 20 }}
+                    transition={{ duration: 0.5, delay: 0.4 }} 
+                    className='text-4xl text-gray-800/95 font-extrabold mb-6 px-32'>
+                        Have Question In Mind? Let Us Help You
+                </motion.h1>
+
+                <motion.div className='flex max-w-2xl gap-4 mt-28 mx-auto'>
+                    <motion.input type='text' placeholder='yourmail@gmail.com' className='py-3 px-4 outline-none focus:border-b-2 text-lg ml-10 w-[96%]'/>
+                    <motion.button className='bg-[#ED573E] text-white font-semibold px-12 py-2 rounded-full text-sm'>Submit</motion.button>
+                </motion.div>
+
+            </motion.div>
+        </section>
+    )
+}
+
+export default ReachOut
