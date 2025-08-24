f72c95416042cd49234d9d9c799a60ed59948948
Preacher-Y <ysheja@gmail.com>
2025-08-24 19:03:44 +0200

    ShoppingMall: - Small adjustment

--- ShoppingMall/src/pages/home.tsx
+++ ShoppingMall/src/pages/home.tsx
@@ -1,6 +1,5 @@
 import About from '../components/about';
 import Footer from '../components/footer';
-import Header from '../components/header';
 import Hero from '../components/hero';
 import How from '../components/how';
 import ReachOut from '../components/reachout';
@@ -10,15 +9,12 @@ import Testimonial from '../components/testmonial';
 function Home(){
     return(
         <>
-            <div className="relative w-full h-screen overflow-x-hidden [&::-webkit-scrollbar]:hidden [-ms-overflow-style:'none'] [scrollbar-width:'none']">
-                <Header />
-                <Hero/>
-                <About/>
-                <How/>
-                <Testimonial/>
-                <ReachOut/>
-                <Footer/>
-            </div>
+            <Hero/>
+            <About/>
+            <How/>
+            <Testimonial/>
+            <ReachOut/>
+            <Footer/>
         </>
     )
 }
