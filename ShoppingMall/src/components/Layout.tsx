6878232f1ac90114b0c1ceb1f981338dc06e50c7
Preacher-Y <ysheja@gmail.com>
2025-08-24 19:31:20 +0200

    ShoppingMall: - Small adjustment

--- ShoppingMall/src/components/Layout.tsx
+++ ShoppingMall/src/components/Layout.tsx
@@ -1,11 +1,14 @@
 import Header from './header';
 import { Outlet } from 'react-router-dom';
+import { useScroll } from '../context/ScrollContext';
 
 function Layout() {
+  const { isScrolled } = useScroll();
+
   return (
     <div className="relative w-full min-h-screen overflow-x-hidden [&::-webkit-scrollbar]:hidden [-ms-overflow-style:'none'] [scrollbar-width:'none']">
       <Header />
-      <main>
+      <main className={`transition-all duration-300 ${isScrolled ? 'pt-24' : ''}`}>
         <Outlet />
       </main>
     </div>
