3b0ac2118b6568718bff47239ad1dc67afaeeaee
Preacher-Y <ysheja@gmail.com>
2025-08-24 13:22:48 +0200

    ShoppingMall: - Small adjustment

--- ShoppingMall/src/components/footer.tsx
+++ ShoppingMall/src/components/footer.tsx
@@ -18,7 +18,7 @@ function Footer(){
                     initial={{ opacity: 0, y: 20 }}
                     animate={isInView ? { opacity: 1, y: 0 } : { opacity: 0, y: 20 }}
                     transition={{ duration: 0.5, delay: 0.2 }}
-                    className='grid gap-12 text-4xl'>
+                    className='grid gap-12 cursor-pointer text-4xl'>
                     
                         <motion.h1
                             initial={{ opacity: 0, y: 20 }}
@@ -36,24 +36,24 @@ function Footer(){
                             <motion.h1 className='icon-[ci--facebook] hover:scale-110 hover:rotate-4 hover:text-blue-900 transition-all duration-500'/>
                         </motion.div>
                 </motion.div>
-                <motion.div className='flex font-semibold gap-10 text-lg'>
+                <motion.div className='flex cursor-pointer font-semibold gap-10 text-lg'>
                     <motion.div>
-                        <motion.h1>Quality</motion.h1>
-                        <motion.h1>Help</motion.h1>
-                        <motion.h1>Share</motion.h1>
-                        <motion.h1>Carrers</motion.h1>
-                        <motion.h1>Work</motion.h1>
-                        <motion.h1>Testamonials</motion.h1>
+                        <motion.h1 className="hover:text-black">Quality</motion.h1>
+                        <motion.h1 className="hover:text-black">Help</motion.h1>
+                        <motion.h1 className="hover:text-black">Share</motion.h1>
+                        <motion.h1 className="hover:text-black">Carrers</motion.h1>
+                        <motion.h1 className="hover:text-black">Work</motion.h1>
+                        <motion.h1 className="hover:text-black">Testamonials</motion.h1>
                     </motion.div>
                     <motion.div>
-                        <motion.h1>244-5333-7783</motion.h1>
-                        <motion.h1>hello@food.com</motion.h1>
-                        <motion.h1>press@food.com</motion.h1>
-                        <motion.h1>contact@food.com</motion.h1>
+                        <motion.h1 className="hover:text-black">244-5333-7783</motion.h1>
+                        <motion.h1 className="hover:text-black">hello@food.com</motion.h1>
+                        <motion.h1 className="hover:text-black">press@food.com</motion.h1>
+                        <motion.h1 className="hover:text-black">contact@food.com</motion.h1>
                     </motion.div>
                     <motion.div>
-                        <motion.h1>Terms & Conditions</motion.h1>
-                        <motion.h1>Privacy Policy</motion.h1>
+                        <motion.h1 className="hover:text-black">Terms & Conditions</motion.h1>
+                        <motion.h1 className="hover:text-black">Privacy Policy</motion.h1>
                     </motion.div>
                 </motion.div>
 
