54969f7bc1b2ddc9483c1b7b83058abf41792af9
Preacher-Y <ysheja@gmail.com>
2025-08-24 13:13:03 +0200

    ShoppingMall: - Small adjustment

--- ShoppingMall/src/components/footer.tsx
+++ ShoppingMall/src/components/footer.tsx
@@ -7,7 +7,7 @@ function Footer(){
     const isInView = useInView(ref, { once: false, amount: 0.3 });
 
     return(
-        <section ref={ref} className='px-32 py-20 mb-30 text-gray-600/80'>
+        <section ref={ref} className='px-32 py-18 text-gray-600/80'>
             <motion.div
                 initial={{ opacity: 0, y: 20 }}
                 animate={isInView ? { opacity: 1, y: 0 } : { opacity: 0, y: 20 }}
