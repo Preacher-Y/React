a5cf8595fd88403592d680b9e5c29dedbbb5bcba
Preacher-Y <ysheja@gmail.com>
2025-08-24 13:19:13 +0200

    ShoppingMall: - Small adjustment

--- ShoppingMall/src/components/footer.tsx
+++ ShoppingMall/src/components/footer.tsx
@@ -30,10 +30,10 @@ function Footer(){
                             animate={isInView ? { opacity: 1, y: 0 } : { opacity: 0, y: 20 }}
                             transition={{ duration: 0.5, delay: 0.2 }}
                             className='flex gap-6 text-5xl'>
-                            <motion.h1 className='icon-[lineicons--x]'/>
-                            <motion.h1 className='icon-[line-md--instagram]'/>
-                            <motion.h1 className='icon-[mdi--youtube] text-6xl -mt-1'/>
-                            <motion.h1 className='icon-[ci--facebook]'/>
+                            <motion.h1 className='icon-[lineicons--x] hover:text-black hover:scale-110 hover:rotate-4 transition-all duration-500'/>
+                            <motion.h1 className='icon-[line-md--instagram] hover:scale-110 hover:rotate-4 transition-all duration-500'/>
+                            <motion.h1 className='icon-[mdi--youtube] text-6xl hover:scale-110 hover:rotate-4 hover:text-red-700 -mt-1 transition-all duration-500'/>
+                            <motion.h1 className='icon-[ci--facebook] hover:scale-110 hover:rotate-4 hover:text-blue-900 transition-all duration-500'/>
                         </motion.div>
                 </motion.div>
                 <motion.div className='flex font-semibold gap-10 text-lg'>
