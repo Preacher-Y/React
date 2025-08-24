5f6493dcd8734a85388b821fc6c5839cd0df7e92
Preacher-Y <ysheja@gmail.com>
2025-08-24 15:14:40 +0200

    ShoppingMall: - Small adjustment

--- ShoppingMall/src/components/header.tsx
+++ ShoppingMall/src/components/header.tsx
@@ -1,32 +1,28 @@
 import { motion } from 'framer-motion';
 import Logo from '../assets/android-app-top-banner.png'
-import Words from '../assets/android-chrome-512x512.png'
 import { memo } from 'react';
 import {HashLink} from 'react-router-hash-link';
+import { Link } from 'react-router-dom';
 
 
 function Header() {
   return (
     <header className="flex z-50 pt-4 justify-between items-center px-32 text-center text-lg font-semibold text-gray-800 [&::-webkit-scrollbar]:hidden [-ms-overflow-style:'none'] [scrollbar-width:'none']">
-      <div className='flex items-center'>
+      <Link to={'/'} className='flex items-center'>
         <motion.img 
             src={Logo} 
             alt="Logo" 
             width={60} 
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
-            transition={{ duration: 0.5, delay: 0.1 , ease: "easeIn" }}
+            transition={{ duration: 0.5, delay: 0.3 , ease: "easeIn" }}
         />
-        <motion.img 
-            src={Words} 
-            alt="words" 
-            width={90} 
-            className='-mb-4'
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            transition={{ duration: 0.5, delay: 0.17 , ease: "easeIn" }}
-        />
-      </div>
+        <motion.h1
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 0.5, delay: 0.35 , ease: "easeIn" }}
+          className="font-['Great_Vibes'] pt-1.5 text-2xl text-[#ED573E]">DuBuy</motion.h1>
+      </Link>
       <nav className='flex gap-8 items-center mt-4'>
         <HashLink to={'#Home'} className='group relative'>
           <button className='group-hover:text-[#ED573E] max-lg:group-hover:text-white group-hover:-translate-y-1.5 transition-all duration-300'>
