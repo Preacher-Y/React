44dd663f747458209353776addff4776811d2695
Preacher-Y <ysheja@gmail.com>
2025-08-24 19:31:14 +0200

    ShoppingMall: - Small adjustment

--- ShoppingMall/src/components/header.tsx
+++ ShoppingMall/src/components/header.tsx
@@ -3,11 +3,18 @@ import Logo from '../assets/android-app-top-banner.png'
 import { memo } from 'react';
 import {HashLink} from 'react-router-hash-link';
 import { Link } from 'react-router-dom';
+import { useScroll } from '../context/ScrollContext';
 
 
 function Header() {
+  const { isScrolled } = useScroll();
+
   return (
-    <header className="flex z-50 pt-4 justify-between items-center px-32 text-center text-lg font-semibold text-gray-800 [&::-webkit-scrollbar]:hidden [-ms-overflow-style:'none'] [scrollbar-width:'none']">
+    <header className={`flex z-50 py-4 justify-between items-center px-32 text-center text-lg font-semibold text-gray-800 [&::-webkit-scrollbar]:hidden [-ms-overflow-style:'none'] [scrollbar-width:'none'] transition-all duration-300 ${
+      isScrolled 
+        ? 'fixed top-0 left-0 right-0 bg-white shadow-lg rounded-b-3xl mx-4' 
+        : 'relative'
+    }`}>
       <Link to={'/'} className='flex items-center'>
         <motion.img 
             src={Logo} 
@@ -25,38 +32,70 @@ function Header() {
       </Link>
       <nav className='flex gap-8 items-center mt-4'>
         <HashLink to={'#Home'} className='group relative'>
-          <button className='group-hover:text-[#ED573E] max-lg:group-hover:text-white group-hover:-translate-y-1.5 transition-all duration-300'>
+          <button className={`group-hover:-translate-y-1.5 transition-all duration-300 ${
+            isScrolled 
+              ? 'group-hover:text-[#ED573E]' 
+              : 'group-hover:text-[#ED573E] max-lg:group-hover:text-white'
+          }`}>
             Home
           </button>
-          <div className='absolute -bottom-2 left-1/2 transform -translate-x-1/2 w-1.5 h-1.5 rounded-full bg-[#ED573E] max-lg:bg-white opacity-0 group-hover:opacity-100 transition-all duration-300'></div>
+          <div className={`absolute -bottom-2 left-1/2 transform -translate-x-1/2 w-1.5 h-1.5 rounded-full opacity-0 group-hover:opacity-100 transition-all duration-300 ${
+            isScrolled ? 'bg-[#ED573E]' : 'bg-[#ED573E] max-lg:bg-white'
+          }`}></div>
         </HashLink>
 
         <HashLink to={'#About'} className='group relative'>
-          <button className='group-hover:text-[#ED573E] max-lg:group-hover:text-white group-hover:-translate-y-1.5 transition-all duration-300'>
+          <button className={`group-hover:-translate-y-1.5 transition-all duration-300 ${
+            isScrolled 
+              ? 'group-hover:text-[#ED573E]' 
+              : 'group-hover:text-[#ED573E] max-lg:group-hover:text-white'
+          }`}>
             About
           </button>
-          <div className='absolute -bottom-2 left-1/2 transform -translate-x-1/2 w-1.5 h-1.5 rounded-full bg-[#ED573E] max-lg:bg-white opacity-0 group-hover:opacity-100 transition-all duration-300'></div>
+          <div className={`absolute -bottom-2 left-1/2 transform -translate-x-1/2 w-1.5 h-1.5 rounded-full opacity-0 group-hover:opacity-100 transition-all duration-300 ${
+            isScrolled ? 'bg-[#ED573E]' : 'bg-[#ED573E] max-lg:bg-white'
+          }`}></div>
         </HashLink>
 
         <HashLink to={'#Testamonial'} className='group relative'>
-          <button className='group-hover:text-white group-hover:-translate-y-1.5 transition-all duration-300'>
+          <button className={`group-hover:-translate-y-1.5 transition-all duration-300 ${
+            isScrolled 
+              ? 'group-hover:text-[#ED573E]' 
+              : 'group-hover:text-white'
+          }`}>
             Testamonials
           </button>
-          <div className='absolute -bottom-2 left-1/2 transform -translate-x-1/2 w-1.5 h-1.5 rounded-full bg-white opacity-0 group-hover:opacity-100 transition-all duration-300'></div>
+          <div className={`absolute -bottom-2 left-1/2 transform -translate-x-1/2 w-1.5 h-1.5 rounded-full opacity-0 group-hover:opacity-100 transition-all duration-300 ${
+            isScrolled ? 'bg-[#ED573E]' : 'bg-white'
+          }`}></div>
         </HashLink>
 
         <HashLink to={'#Footer'} className='group relative'>
-          <button className='group-hover:text-white group-hover:-translate-y-1.5 transition-all duration-300'>
+          <button className={`group-hover:-translate-y-1.5 transition-all duration-300 ${
+            isScrolled 
+              ? 'group-hover:text-[#ED573E]' 
+              : 'group-hover:text-white'
+          }`}>
             Contact
           </button>
-          <div className='absolute -bottom-2 left-1/2 transform -translate-x-1/2 w-1.5 h-1.5 rounded-full bg-white opacity-0 group-hover:opacity-100 transition-all duration-300'></div>
+          <div className={`absolute -bottom-2 left-1/2 transform -translate-x-1/2 w-1.5 h-1.5 rounded-full opacity-0 group-hover:opacity-100 transition-all duration-300 ${
+            isScrolled ? 'bg-[#ED573E]' : 'bg-white'
+          }`}></div>
         </HashLink>
 
         <Link to={'cart'} className='items-center flex'>
-          <h1 className='icon-[mdi--cart-outline] text-[22px] hover:text-white'/>
+          <h1 className={`icon-[mdi--cart-outline] text-[22px] transition-all duration-300 ${
+            isScrolled 
+              ? 'hover:text-[#ED573E]' 
+              : 'hover:text-white'
+          }`}/>
         </Link>
         
-        <Link to={'buy'} className='bg-white px-4 py-2 rounded-full hover:bg-[#ED573E] hover:shadow-[0px_0px_20px] hover:text-white hover:ring-2 transition-all duration-700 ease-in-out'>
+        <Link to={'buy'} className={`px-4 py-2 rounded-full transition-all duration-700 ease-in-out ${
+          isScrolled 
+            ? 'bg-[#ED573E] text-white hover:bg-[#ED573E]/90 hover:shadow-[0px_0px_20px] hover:ring-2' 
+            : 'bg-white hover:bg-[#ED573E] hover:shadow-[0px_0px_20px] hover:text-white hover:ring-2'
+        }`}>
           Order Now
         </Link>
       </nav>
