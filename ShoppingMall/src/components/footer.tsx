47916748161e59d41afdfd7e1e856dc6f12097f4
Preacher-Y <ysheja@gmail.com>
2025-08-24 11:25:09 +0200

    ShoppingMall: - Small adjustment

--- ShoppingMall/src/components/footer.tsx
+++ ShoppingMall/src/components/footer.tsx
@@ -7,14 +7,32 @@ function Footer(){
     const isInView = useInView(ref, { once: false, amount: 0.3 });
 
     return(
-        <section className='px-32 py-20 mb-10'>
+        <section ref={ref} className='px-32 py-20 mb-30 text-gray-600/80'>
             <motion.div
                 initial={{ opacity: 0, y: 20 }}
                 animate={isInView ? { opacity: 1, y: 0 } : { opacity: 0, y: 20 }}
-                transition={{ duration: 0.5, delay: 0.2 }}>
+                transition={{ duration: 0.5, delay: 0.2 }}
+                className='flex justify-center items-center'>
+
+                <motion.div
+                    initial={{ opacity: 0, y: 20 }}
+                    animate={isInView ? { opacity: 1, y: 0 } : { opacity: 0, y: 20 }}
+                    transition={{ duration: 0.5, delay: 0.2 }}>
                     
-                <motion.div>
-    
+                        <motion.h1
+                            initial={{ opacity: 0, y: 20 }}
+                            animate={isInView ? { opacity: 1, y: 0 } : { opacity: 0, y: 20 }}
+                            transition={{ duration: 0.5, delay: 0.2 }} 
+                            className="font-['Great_Vibes']">DuBuy</motion.h1>
+                        <motion.div
+                            initial={{ opacity: 0, y: 20 }}
+                            animate={isInView ? { opacity: 1, y: 0 } : { opacity: 0, y: 20 }}
+                            transition={{ duration: 0.5, delay: 0.2 }}>
+                            <motion.h1 className='icon-[lineicons--x]'/>
+                            <motion.h1 className='icon-[line-md--instagram]'/>
+                            <motion.h1 className='icon-[mdi--youtube]'/>
+                            <motion.h1 className='icon-[ci--facebook]'/>
+                        </motion.div>
                 </motion.div>
 
             </motion.div>
