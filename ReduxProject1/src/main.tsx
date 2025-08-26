8ec331b7ebabae0c82a41f8486efc489dc3ff082
Preacher-Y <ysheja@gmail.com>
2025-08-26 13:58:06 +0200

    ReduxProject1: - Added the reducer from the userSlice

--- ReduxProject1/src/main.tsx
+++ ReduxProject1/src/main.tsx
@@ -2,9 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { configureStore } from '@reduxjs/toolkit'
+import { Provider } from 'react-redux'
+import userReducer from './features/user.ts'
+
+const store = configureStore({reducer:{userReducer}})
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </StrictMode>,
 )
