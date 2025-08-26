6c4649974ed620be587d8fd2d1c7a858efff136b
Preacher-Y <ysheja@gmail.com>
2025-08-26 14:00:05 +0200

    ReduxProject1: - Small change

--- ReduxProject1/src/main.tsx
+++ ReduxProject1/src/main.tsx
@@ -6,7 +6,11 @@ import { configureStore } from '@reduxjs/toolkit'
 import { Provider } from 'react-redux'
 import userReducer from './features/user.ts'
 
-const store = configureStore({reducer:{userReducer}})
+const store = configureStore({
+  reducer:{
+    user:userReducer
+  }
+})
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
