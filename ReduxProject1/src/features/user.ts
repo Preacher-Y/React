36dfa44967495cba3278d03476791dbc11afda36
Preacher-Y <ysheja@gmail.com>
2025-08-26 18:39:38 +0200

    ReduxProject1: - Small change

--- ReduxProject1/src/features/user.ts
+++ ReduxProject1/src/features/user.ts
@@ -1,13 +1,24 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, type PayloadAction } from "@reduxjs/toolkit";
+
+export type userType={
+    name: string;
+    profession: string;
+    number: string;
+    email: string;
+    address: string;
+}
+
+const initialValue:userType = {name:'', profession:'',number:'', email:'', address:''}
 
 export const userSlice = createSlice({
     name: 'user',
-    initialState: {value:{name:'Yves Sheja N. M.', profession:'Full Stack Engineer', profile:'https://i.pravatar.cc/150?img=2',number:'+250 788 123 456', email:'ysheja@gmail.com', address:'Kigali, Rwanda'}},
+    initialState: {value:initialValue},
     reducers:{
-        Login:(state,action)=>{
+        login:(state,action:PayloadAction<userType>)=>{
             state.value=action.payload
         }
     }
 })
 
+export const {login} = userSlice.actions
 export default userSlice.reducer
