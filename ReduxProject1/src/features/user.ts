1304b1bf4af3bfb730379c97d07ca4d3d317119e
Preacher-Y <ysheja@gmail.com>
2025-08-26 15:32:36 +0200

    ReduxProject1: - Small change

--- ReduxProject1/src/features/user.ts
+++ ReduxProject1/src/features/user.ts
@@ -2,7 +2,7 @@ import { createSlice } from "@reduxjs/toolkit";
 
 export const userSlice = createSlice({
     name: 'user',
-    initialState: {value:{name:'Yves Sheja N. M.', profession:'Full Stack Engineer', profile:'https://i.pravatar.cc/',number:'+250 788 123 456', email:'ysheja@gmail.com', address:'Kigali, Rwanda'}},
+    initialState: {value:{name:'Yves Sheja N. M.', profession:'Full Stack Engineer', profile:'https://i.pravatar.cc/150?img=2',number:'+250 788 123 456', email:'ysheja@gmail.com', address:'Kigali, Rwanda'}},
     reducers:{
         Login:(state,action)=>{
             state.value=action.payload
