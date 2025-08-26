8e66f6c746a56409895d88db80d6113828e2c52e
Preacher-Y <ysheja@gmail.com>
2025-08-26 09:48:22 +0200

    React Router Project: - Firebase installation

---
+++ ReactRouteProject/src/firebase.ts
@@ -0,0 +1,15 @@
+
+import { initializeApp } from "firebase/app";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAFwV7Qdtzye2ffgqF8As02QpJjt5uiBZo",
+  authDomain: "vanlifepreacher.firebaseapp.com",
+  projectId: "vanlifepreacher",
+  storageBucket: "vanlifepreacher.firebasestorage.app",
+  messagingSenderId: "114261670338",
+  appId: "1:114261670338:web:43f818d44c87aac13e7e5b",
+  measurementId: "G-QST46V0TG0"
+};
+
+export const app = initializeApp(firebaseConfig);
