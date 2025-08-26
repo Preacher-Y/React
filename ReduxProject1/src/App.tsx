e24eb8bcd5317c9091b2ebd2aeb38e6a128a0afb
Preacher-Y <ysheja@gmail.com>
2025-08-26 17:50:59 +0200

    ReduxProject1: - Small change

--- ReduxProject1/src/App.tsx
+++ ReduxProject1/src/App.tsx
@@ -1,13 +1,22 @@
-import { createBrowserRouter, createRoutesFromElements, Outlet, Route, RouterProvider } from 'react-router-dom';
+import { createBrowserRouter, createRoutesFromElements, Navigate, Outlet, Route, RouterProvider } from 'react-router-dom';
 import Header from './components/Login';
 import Profile from './components/Profile';
+
+function ProtectedRoute({ children }: { children: React.JSX.Element }) {
+  const isLoggedIn = localStorage.getItem('LoggedInRedux')?.toLowerCase() === 'true';
+  
+  if (!isLoggedIn) {
+    return <Navigate to="/" state={{ message: "You must first Login " }} replace />
+  }
+  return children;
+}
 
 function App() {
 
   const router = createBrowserRouter(createRoutesFromElements(
     <Route path='/' element={<Outlet/>}>
       <Route index element={<Header/>}/>
-      <Route path='profile' element={<Profile/>}/>
+      <Route path='profile' element={<ProtectedRoute><Profile/></ProtectedRoute>}/>
     </Route>
   ))
 
