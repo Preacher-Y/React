b3012e4b788d401f0ca6849723510db377fc701a
Preacher-Y <ysheja@gmail.com>
2025-08-27 13:38:43 +0200

    ReduxProject2: - Small Change

--- ReduxProject2-todo/src/main.tsx
+++ ReduxProject2-todo/src/main.tsx
@@ -2,9 +2,20 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { configureStore } from '@reduxjs/toolkit'
+import { Provider } from 'react-redux'
+import todoReducer from './features/todoListSlice'
+
+const store = configureStore({
+  reducer: {
+    todoList: todoReducer
+  }
+})
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </StrictMode>,
 )
