75337344d05d2d0252f2db01da11a4cd47a04f80
Preacher-Y <ysheja@gmail.com>
2025-08-28 12:42:10 +0200

    ReduxProject2: - Small Change

---
+++ ReduxProject2-todo/src/components/header.tsx
@@ -0,0 +1,18 @@
+import clsx from "clsx"
+import { NavLink } from "react-router-dom"
+
+
+function header() {
+
+  const normal = "underline-offset-3 text-sm hover:text-zinc-300 hover:underline"
+  const active = "text-zinc-400 underline"
+
+  return (
+    <header className="bg-zinc-50 dark:bg-zinc-800 text-zinc-900 flex justify-center gap-10 py-4 dark:text-zinc-50">
+        <NavLink to={'/'} className={({ isActive }) => clsx(normal, isActive && active)}>TodoList App</NavLink>
+        <NavLink to={'fetch'} className={({ isActive }) => clsx(normal, isActive && active)}>FetchUser App</NavLink>
+    </header>
+  )
+}
+
+export default header
